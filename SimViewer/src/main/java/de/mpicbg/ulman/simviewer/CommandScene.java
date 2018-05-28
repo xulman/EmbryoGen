0aa507f442094a573a3557e5a9d3a018d01491bf
Vladimír Ulman <ulman@mpi-cbg.de>
2018-05-28 17:15:53 +0200

    FIX: CommandScene can be interrupted immediately (without waiting for key stroke).

--- SimViewer/src/main/java/de/mpicbg/ulman/simviewer/CommandScene.java
+++ SimViewer/src/main/java/de/mpicbg/ulman/simviewer/CommandScene.java
@@ -1,7 +1,8 @@
 package de.mpicbg.ulman.simviewer;
 
+import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 import de.mpicbg.ulman.simviewer.DisplayScene;
 
 /**
@@ -18,47 +19,62 @@ public class CommandScene implements Runnable
 		scene = _scene;
 	}
 
+	/** reference on the controlled rendering display */
 	private final DisplayScene scene;
+
 
 	/** reads the console and dispatches the commands */
 	public void run()
 	{
+		System.out.println("Key listener: Started.");
+		final InputStreamReader console = new InputStreamReader(System.in);
 		try {
-			while (Thread.interrupted() == false)
+			while (true)
 			{
-				int key = System.in.read();
-				switch (key)
-				{
-				case 'h':
-					System.out.println("List of accepted commands:");
-					System.out.println("h - Shows this help message");
-					System.out.println("q - Quits the program");
-					System.out.println("a - Toggles display of the axes in the scene centre");
-					System.out.println("b - Toggles display of the scene border");
-					System.out.println("c - Toggles display of the cells");
-					break;
-
-				case 'q':
-					scene.stop();
-					break;
-
-				case 'a':
-					scene.ToggleDisplayAxes();
-					break;
-				case 'b':
-					scene.ToggleDisplaySceneBorder();
-					break;
-				case 'c':
-					scene.UpdateCell();
-					break;
-				}
+				if (console.ready())
+					processKey(console.read());
+				else
+					Thread.sleep(1000);
 			}
 		}
 		catch (IOException e) {
-			System.out.print("Key listener: Error reading the console.");
+			System.out.println("Key listener: Error reading the console.");
 			e.printStackTrace();
 		}
+		catch (InterruptedException e) {
+			System.out.println("Key listener: Stopped.");
+		}
+	}
 
-		System.out.print("Key listener: Stopped.");
+
+	private
+	void processKey(final int key) throws InterruptedException
+	{
+		switch (key)
+		{
+		case 'h':
+			System.out.println("List of accepted commands:");
+			System.out.println("h - Shows this help message");
+			System.out.println("q - Quits the program");
+			System.out.println("a - Toggles display of the axes in the scene centre");
+			System.out.println("b - Toggles display of the scene border");
+			System.out.println("c - Toggles display of the cells");
+			break;
+
+		case 'a':
+			scene.ToggleDisplayAxes();
+			break;
+		case 'b':
+			scene.ToggleDisplaySceneBorder();
+			break;
+		case 'c':
+			scene.UpdateCell();
+			break;
+
+		case 'q':
+			scene.stop();
+			//don't wait for GUI to tell me to stop
+			throw new InterruptedException("Stop myself now.");
+		}
 	}
 }
