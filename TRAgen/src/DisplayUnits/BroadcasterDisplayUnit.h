0fdca50873ef8ec4cef1ea898e095d76efc87931
Vladimír Ulman <ulman@mpi-cbg.de>
2018-05-29 17:24:37 +0200

    T ADD: c++11 std in Cmake, BroadcasterDisplayUnit added

---
+++ TRAgen/src/DisplayUnits/BroadcasterDisplayUnit.h
@@ -0,0 +1,76 @@
+#ifndef BROADCASTERDISPLAYUNIT_H
+#define BROADCASTERDISPLAYUNIT_H
+
+#include "DisplayUnit.h"
+#include "../params.h"
+#include <forward_list>
+
+/**
+ * This class is essentially a repeater that repeats the drawing
+ * commands on all 'DisplayUnit's that are registered with an
+ * instance of this class.
+ *
+ * Author: Vladimir Ulman, 2018
+ */
+class BroadcasterDisplayUnit : public DisplayUnit
+{
+public:
+	void DrawPoint(const Vector3d<float>& pos,
+	               const float radius = 1.0f,
+	               const int color = 0)
+	{
+		for (auto it = displayUnits.begin(); it != displayUnits.end(); ++it)
+			(*it)->DrawPoint(pos,radius,color);
+	}
+
+	void DrawLine(const Vector3d<float>& posA,
+	              const Vector3d<float>& posB,
+	              const int color = 0)
+	{
+		for (auto it = displayUnits.begin(); it != displayUnits.end(); ++it)
+			(*it)->DrawLine(posA,posB,color);
+	}
+
+	void DrawVector(const Vector3d<float>& pos,
+	                const Vector3d<float>& vector,
+	                const int color = 0)
+	{
+		for (auto it = displayUnits.begin(); it != displayUnits.end(); ++it)
+			(*it)->DrawVector(pos,vector,color);
+	}
+
+	void DrawTriangle(const Vector3d<float>& posA,
+	                  const Vector3d<float>& posB,
+	                  const Vector3d<float>& posC,
+	                  const int color = 0)
+	{
+		for (auto it = displayUnits.begin(); it != displayUnits.end(); ++it)
+			(*it)->DrawTriangle(posA,posB,posC,color);
+	}
+
+
+	///variants with reference params
+	void RegisterUnit(DisplayUnit& ds)
+	{
+		displayUnits.push_front(&ds);
+	}
+	void UnregisterUnit(DisplayUnit& ds)
+	{
+		displayUnits.remove(&ds);
+	}
+
+	///variants with pointer params
+	void RegisterUnit(DisplayUnit* ds)
+	{
+		displayUnits.push_front(ds);
+	}
+	void UnregisterUnit(DisplayUnit* ds)
+	{
+		displayUnits.remove(ds);
+	}
+
+private:
+	/** local list of who/where to send the drawing requests */
+	std::forward_list<DisplayUnit*> displayUnits;
+};
+#endif
