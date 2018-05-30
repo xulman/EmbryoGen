36db0ddc97469d3a9040c688994374609f542642
Vladimír Ulman <ulman@mpi-cbg.de>
2018-05-31 00:06:11 +0200

    S ADD: fake cells (5x5 grid) to have something to display

--- SimViewer/src/main/java/de/mpicbg/ulman/simviewer/CommandScene.java
+++ SimViewer/src/main/java/de/mpicbg/ulman/simviewer/CommandScene.java
@@ -60,6 +60,7 @@ public class CommandScene implements Runnable
 			System.out.println("a - Toggles display of the axes in the scene centre");
 			System.out.println("b - Toggles display of the scene border");
 			System.out.println("c - Toggles display of the cells");
+			System.out.println("f - Adds some cells to have something to display");
 			System.out.println("d - Deletes all cells (even if not displayed)");
 			System.out.println("r,R - decreases/increases radius of all cells by 0.2");
 			System.out.println("x,X - moves left/right x-position of all cells by 0.2");
@@ -73,6 +74,9 @@ public class CommandScene implements Runnable
 			break;
 		case 'c':
 			scene.ToggleDisplayCells();
+			break;
+		case 'f':
+			CreateFakeCells();
 			break;
 		case 'd':
 			scene.RemoveCells();
@@ -116,6 +120,41 @@ public class CommandScene implements Runnable
 			scene.stop();
 			//don't wait for GUI to tell me to stop
 			throw new InterruptedException("Stop myself now.");
+		}
+	}
+
+
+	void CreateFakeCells()
+	{
+		final float xStep = scene.sceneSize[0] / 6.0f;
+		final float yStep = scene.sceneSize[1] / 6.0f;
+
+		final float xCentre  = scene.sceneOffset[0] + 0.5f*scene.sceneSize[0];
+		final float yCentre  = scene.sceneOffset[1] + 0.5f*scene.sceneSize[1];
+		final float zCentre  = scene.sceneOffset[2] + 0.5f*scene.sceneSize[2];
+
+		for (int y=0; y < 5; ++y)
+		for (int x=0; x < 5; ++x)
+		{
+			//if (x != 2 && y != 2)
+			{
+				final Cell c = new Cell(2,0);
+				c.ID = x+10*y;
+				c.sphereRadii[0]  = 3.0f;
+				c.sphereColors[0] = 2;
+				c.sphereCentres[0] = xCentre + xStep*(x-2.0f) -2.0f;
+				c.sphereCentres[1] = yCentre + yStep*(y-2.0f);
+				c.sphereCentres[2] = zCentre - 1.0f;
+
+				c.sphereRadii[1]  = 3.0f;
+				c.sphereColors[1] = 3;
+				c.sphereCentres[3] = xCentre + xStep*(x-2.0f) +2.0f;
+				c.sphereCentres[4] = yCentre + yStep*(y-2.0f);
+				c.sphereCentres[5] = zCentre + 1.0f;
+
+				scene.cellsData.put(c.ID, c);
+				scene.UpdateCellNodes(c);
+			}
 		}
 	}
 }
