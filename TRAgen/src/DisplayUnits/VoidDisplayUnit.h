130649b020fa20bb7a874e207361667a301bd81e
Vladimír Ulman <ulman@mpi-cbg.de>
2018-06-06 12:43:34 +0200

    T ADD: VoidDisplayUnit, that can be default unit in cells/agents (and        reference instead of pointer can be used)

---
+++ TRAgen/src/DisplayUnits/VoidDisplayUnit.h
@@ -0,0 +1,41 @@
+#ifndef VOIDDISPLAYUNIT_H
+#define VOIDDISPLAYUNIT_H
+
+#include "DisplayUnit.h"
+
+/**
+ * This class implements drawing by not doing anything at all,
+ * this is essentially a placeholder unit when no drawing output
+ * is desired at all.
+ *
+ * Author: Vladimir Ulman, 2018
+ */
+class VoidDisplayUnit : public DisplayUnit
+{
+public:
+	void DrawPoint(const int ID,
+	               const Vector3d<float>& pos,
+	               const float radius = 1.0f,
+	               const int color = 0)
+	{ }
+
+	void DrawLine(const int ID,
+	              const Vector3d<float>& posA,
+	              const Vector3d<float>& posB,
+	              const int color = 0)
+	{ }
+
+	void DrawVector(const int ID,
+	                const Vector3d<float>& pos,
+	                const Vector3d<float>& vector,
+	                const int color = 0)
+	{ }
+
+	void DrawTriangle(const int ID,
+	                  const Vector3d<float>& posA,
+	                  const Vector3d<float>& posB,
+	                  const Vector3d<float>& posC,
+	                  const int color = 0)
+	{ }
+};
+#endif
