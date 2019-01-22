318122c7be9b7ef0b1b7925a7f6c95cd4789dc98
Vladimír Ulman <ulman@mpi-cbg.de>
2019-01-22 14:03:18 +0100

    CHG,ADD: plugin front-end is ready/finished

--- SimViewer/src/main/java/de/mpicbg/ulman/simviewer_FijiPlugin/simviewer_FijiPlugin.java
+++ SimViewer/src/main/java/de/mpicbg/ulman/simviewer_FijiPlugin/simviewer_FijiPlugin.java
@@ -10,19 +10,13 @@
 package de.mpicbg.ulman.simviewer_FijiPlugin;
 
 import org.scijava.command.Command;
-import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.log.LogService;
-import net.imagej.ImageJ;
 
-@Plugin(type = Command.class, menuPath = "Ulman>SimViewer",
+@Plugin(type = Command.class, menuPath = "Plugins>EmbryoGen>SimViewer",
         name = "EmbryoGen_SimViewerPlugin", headless = true,
 		  description = "Fires up the de.mpicbg.ulman.simviewer, a Scenery viewer of the running EmbryoGen simulation.")
 public class simviewer_FijiPlugin implements Command
 {
-	@Parameter
-	private LogService log;
-
 	@Override
 	public void run()
 	{
@@ -32,24 +26,14 @@ public class simviewer_FijiPlugin implements Command
 	public static
 	void runSimViewer()
 	{
-		System.out.println("hello world");
+		System.out.println("starting the EmbryoGen's SimViewer...");
+		de.mpicbg.ulman.simviewer.StartUpScene.main();
 	}
 
 	public static void main(final String... args)
 	{
 		//start up our own ImageJ instance
-		final ImageJ ij = new net.imagej.ImageJ();
-		ij.ui().showUI();
-
-		//run this class as if from GUI
-		/*
-		ij.command().run(plugin_CTCmeasuresICT.class, true, "gtPath",args[0], "resPath",args[1],
-			"calcTRA",true, "calcSEG",true);
-
-		//and close the IJ instance...
-		//ij.appEvent().quit();
-		*/
-
+		new net.imagej.ImageJ(); //.showUI();
 		runSimViewer();
 	}
 }
