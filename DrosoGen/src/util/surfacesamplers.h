786a569a475196b1d35412840de02ea86900d00d
Vladimír Ulman <ulman@mpi-cbg.de>
2019-04-30 17:40:45 +0200

    ADD: util/surfacesamplers.ImageSampler class

--- DrosoGen/src/util/surfacesamplers.h
+++ DrosoGen/src/util/surfacesamplers.h
@@ -2,6 +2,8 @@
 #define SURFACESAMPLERS_H
 
 #include <math.h>
+#include <i3d/image3d.h>
+#include <functional>
 
 /**
  * This iteratively gives vectors/coordinates that (somewhat) regularly
@@ -100,6 +102,119 @@ private:
 		climberRad = 0;
 		climberRadStep = stepSize / radius;
 		rad = 0;
+	}
+};
+
+
+/**
+ * This iteratively gives vectors/coordinates that (somewhat) regularly
+ * sample a surface (represented with a given voxel value) stored/rasterized
+ * in an image. It essentially (and iteratively) will give vectors/coordinates
+ * of centre of the pixels that satisfy user-given condition.
+ *
+ * Init the sampler with a call to any of the reset*() methods and
+ * then keep getting surface points via next() until it returns with false.
+ *
+ * Note: ST - sampling type, VT - voxel type
+ */
+template <typename ST, typename VT>
+class ImageSampler
+{
+public:
+	void resetByVoxelStep(const i3d::Image3d<VT>& img,
+	                      const std::function<bool(const VT)>& pxPredicate,
+	                      const Vector3d<size_t>& pixelStep)
+	{
+		theCommonInit(img); //sets up the 'res'
+
+		this->pxPredicate = &pxPredicate;
+		this->micronStep.from(pixelStep).elemDivBy(res);
+
+		DEBUG_REPORT("imgSize=" << img.GetSize() << " px, micronStep=" << this->micronStep << " um");
+	}
+
+	void resetByMicronStep(const i3d::Image3d<VT>& img,
+	                       const std::function<bool(const VT)>& pxPredicate,
+	                       const Vector3d<float>& micronStep)
+	{
+		theCommonInit(img);
+
+		this->pxPredicate = &pxPredicate;
+		this->micronStep = micronStep;
+
+		DEBUG_REPORT("imgSize=" << img.GetSize() << " px, micronStep=" << this->micronStep << " um");
+	}
+
+	/** gives another point on the sphere's surface, returns false if
+	    all surface points were examined */
+	bool next(Vector3d<ST>& surfacePoint)
+	{
+		do {
+			while (p != pL && (*pxPredicate)(*p) == false) ++p;
+			if (p == pL) return false;
+
+			//define surfacePoint from the (p-pF) offset
+			size_t offset = (unsigned)(p-pF);
+			tmp.z = offset / xyPlane;
+
+			offset -= tmp.z * xyPlane;
+			tmp.y = offset / xLine;
+
+			offset -= tmp.y * xLine;
+			tmp.x = offset;
+
+			surfacePoint.toMicronsFrom(tmp, res,off);
+			++p;
+
+			/* this is the closed-form of the algorithm:
+			while (p != pL)
+			{
+				if (pxPredicate(*p))
+				{
+					//define surfacePoint from the (p-pF) offset...
+				}
+				++p;
+			}
+			*/
+
+			//is the new point sufficiently far from the previous one?
+			tmpB  = lastSurfacePoint;
+			tmpB -= surfacePoint;
+			tmpB.elemAbs();
+
+			//"sufficiently far" means, per axis:
+			//  is there a move?  if so, is it large enough?
+		} while ((tmpB.x > 0 && tmpB.x < micronStep.x) ||
+		         (tmpB.y > 0 && tmpB.y < micronStep.y) ||
+		         (tmpB.z > 0 && tmpB.z < micronStep.z));
+
+		lastSurfacePoint = surfacePoint;
+		return true;
+	}
+
+private:
+	const std::function<bool(const VT)>* pxPredicate;
+	Vector3d<ST> micronStep;
+
+	Vector3d<ST> res,off;
+	size_t xLine,xyPlane;
+	const VT* p,* pF,* pL;
+
+	Vector3d<size_t> tmp;
+	Vector3d<ST> lastSurfacePoint, tmpB;
+
+	void theCommonInit(const i3d::Image3d<VT>& img)
+	{
+		res.fromI3dVector3d(img.GetResolution().GetRes());
+		off.fromI3dVector3d(img.GetOffset());
+
+		xLine = img.GetSizeX();
+		xyPlane = xLine * img.GetSizeY();
+
+		p = pF = img.GetFirstVoxelAddr();
+		pL = pF + img.GetImageSize();
+
+		lastSurfacePoint = -TOOFAR;
 	}
 };
 #endif
