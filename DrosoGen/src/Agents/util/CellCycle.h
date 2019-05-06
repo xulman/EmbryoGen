a0d40398994ab0ca1a9b3c8d49016f65eef60dd0
Vladimír Ulman <ulman@mpi-cbg.de>
2019-05-06 17:36:04 +0200

    ADD: CellCycle API is ready

--- DrosoGen/src/Agents/util/CellCycle.h
+++ DrosoGen/src/Agents/util/CellCycle.h
@@ -6,6 +6,7 @@
 /** A datatype enumerating the particular phases of cell cycle in their order */
 typedef enum
 {
+	newBorn=-1,
 	G1Phase=0,
 	SPhase=1,
 	G2Phase=2,
@@ -13,19 +14,20 @@ typedef enum
 	Metaphase=4,
 	Anaphase=5,
 	Telophase=6,
-	Cytokinesis=7
+	Cytokinesis=7,
+	RestInPeace=8
 } ListOfPhases;
 
 
 /**
  * Utility class that governs proper cycling through a (normal) cell cycle.
  *
- * Derive this class while overriding the default methods for
- * the basic 8 phases (aka phase_methods) of the cell cycle; instantiate
- * this new class with the desired full cycle duration and the information
- * about the current time (essentially the time when this cell was born),
- * and iteratively keep calling doNextPhase(currentTime) that will assure
- * that the relevant phase_methods are called in the correct order.
+ * Derive this class while overriding the default methods for the basic eight
+ * phases (aka phase_methods) of the cell cycle; instantiate this new class with
+ * the desired full cycle duration via c'tors and the information about the
+ * current time (mostly the time when this cell was born) via startCycling(),
+ * and iteratively keep calling triggerCycleMethods(currentTime) that will
+ * assure that the relevant phase_methods are called in the correct order.
  *
  * Each phase_method has one parameter (phase progress bar ratio) that informs
  * its content how far it should get within the phase; the ratio is always (0;1].
@@ -33,6 +35,9 @@ typedef enum
 class CellCycle
 {
 public:
+	// --------------------------------------------------
+	// construction & phase durations
+
 	/** constructor with default 24h cell cycle */
 	CellCycle():
 		CellCycle( 24*60 ) {}
@@ -44,17 +49,17 @@ public:
 
 	/** constructor with (in minutes) given cycle length */
 	CellCycle(const float _fullCycleDuration):
-		fullCycleDuration(_fullCycleDuration)
-	{
-		determinePhaseDurations();
-	}
+		fullCycleDuration(_fullCycleDuration) {}
+		//NB: overriding determinePhaseDurations() are not-visible in the c'tor,
+		//    hence, this call is postponed into startCycling(), and this method
+		//    was made unavoidable as it is the only that provides the global time
 
 	/** the (informative) cell cycle length [min] */
 	const float fullCycleDuration;
 
 protected:
 	/** the length of individual cell cycle phases [min] */
-	float phaseDurations[8];
+	float phaseDurations[8] = {0,0,0,0,0,0,0,0};
 
 	/** override-able pie-slicing of the fullCycleDuration, this
 	    method is called from every constructor
@@ -103,6 +108,11 @@ public:
 		REPORT("full cycle duration is: " << fullCycleDuration << " minutes ("
 		       << fullCycleDuration/60 << " hrs)");
 
+		float phaseTotalTime = 0;
+		for (int i=G1Phase; i <= Cytokinesis; ++i)
+			phaseTotalTime += phaseDurations[i];
+		REPORT("all cycles duration is: " << phaseTotalTime << " minutes");
+
 		REPORT("  G1Phase    : " << phaseDurations[G1Phase    ] << " mins\t ("
 		       << phaseDurations[G1Phase    ]/fullCycleDuration << "%)");
 		REPORT("  SPhase     : " << phaseDurations[SPhase     ] << " mins\t ("
@@ -120,5 +130,228 @@ public:
 		REPORT("  Cytokinesis: " << phaseDurations[Cytokinesis] << " mins\t ("
 		       << phaseDurations[Cytokinesis]/fullCycleDuration << "%)");
 	}
+
+	// --------------------------------------------------
+	// cycling through the cell cycle phases
+
+protected:
+	/** the currently being processed cycle phase */
+	ListOfPhases curPhase = newBorn;
+
+	/** the global time when the phase now in 'curPhase' has started */
+	float lastPhaseChangeGlobalTime = -1;
+
+public:
+	/** this one actually starts the cycling, provide it with the
+	    current "wall time" (in 'currentGlobalTime') */
+	void startCycling(const float currentGlobalTime)
+	{
+		if (curPhase != newBorn)
+			throw ERROR_REPORT("Re-initializing already initialized cell cycle");
+
+		//define the phase durations
+		determinePhaseDurations();
+		//NB: overriding determinePhaseDurations() are not-visible in the c'tor,
+		//    hence, this call is postponed into startCycling(), and this method
+		//    was made unavoidable as it is the only that provides the global time
+
+		lastPhaseChangeGlobalTime = currentGlobalTime;
+		curPhase = G1Phase;
+		startG1Phase();
+	}
+
+	/** this one calls the phase_methods (see docs of this class) in the correct order,
+	    until the cycle progresses to the given "wall time" (in 'currentGlobalTime') */
+	void triggerCycleMethods(const float currentGlobalTime)
+	{
+		if (curPhase == newBorn)
+			throw ERROR_REPORT("Not yet fully initialized cell cycle");
+
+		//beyond the cell cycle, do nothing
+		if (curPhase == RestInPeace) return;
+
+		bool tryNextPhase = true;
+		while (tryNextPhase)
+		{
+			const float progress = (currentGlobalTime - lastPhaseChangeGlobalTime)
+			                     / phaseDurations[curPhase];
+			DEBUG_REPORT("progress=" << progress);
+
+			if (progress < 0)
+			{
+				throw ERROR_REPORT("lastPhaseChange is ahead given currentGlobalTime, quitting confusedly");
+			}
+			else if (progress == 0)
+			{
+				//there's really no progress required from us, just skip this call
+				DEBUG_REPORT("skipping this call...");
+				tryNextPhase = false;
+			}
+			else if (progress < 1)
+			{
+				//call normally as some progress is required
+				switch (curPhase)
+				{
+				case G1Phase:
+					runG1Phase(progress);
+					break;
+
+				case SPhase:
+					runSPhase(progress);
+					break;
+
+				case G2Phase:
+					runG2Phase(progress);
+					break;
+
+				case Prophase:
+					runProphase(progress);
+					break;
+
+				case Metaphase:
+					runMetaphase(progress);
+					break;
+
+				case Anaphase:
+					runAnaphase(progress);
+					break;
+
+				case Telophase:
+					runTelophase(progress);
+					break;
+
+				case Cytokinesis:
+					runCytokinesis(progress);
+					break;
+
+				default:
+					throw ERROR_REPORT("Unknown cycle state!");
+				}
+
+				//couldn't cross the phase boundaries, yet has done its portion of the work
+				tryNextPhase = false;
+			}
+			else
+			{
+				//got over a boundary between phases, then
+				//call normally to finish to reach 1.0, close, incr. curPhase, open
+				switch (curPhase)
+				{
+				case G1Phase:
+					runG1Phase(1.0);
+					closeG1Phase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = SPhase;
+					startSPhase();
+					break;
+
+				case SPhase:
+					runSPhase(1.0);
+					closeSPhase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = G2Phase;
+					startG2Phase();
+					break;
+
+				case G2Phase:
+					runG2Phase(1.0);
+					closeG2Phase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = Prophase;
+					startProphase();
+					break;
+
+				case Prophase:
+					runProphase(1.0);
+					closeProphase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = Metaphase;
+					startMetaphase();
+					break;
+
+				case Metaphase:
+					runMetaphase(1.0);
+					closeMetaphase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = Anaphase;
+					startAnaphase();
+					break;
+
+				case Anaphase:
+					runAnaphase(1.0);
+					closeAnaphase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = Telophase;
+					startTelophase();
+					break;
+
+				case Telophase:
+					runTelophase(1.0);
+					closeTelophase();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = Cytokinesis;
+					startCytokinesis();
+					break;
+
+				case Cytokinesis:
+					runCytokinesis(1.0);
+					closeCytokinesis();
+					//
+					lastPhaseChangeGlobalTime += phaseDurations[curPhase];
+					curPhase = RestInPeace;
+					break;
+
+				default:
+					throw ERROR_REPORT("Unknown cycle state!");
+				}
+
+				//it might, however, still not be the end of this:
+				//if lastPhaseChangeGlobalTime is still "multiple" whole phases
+				//beyond the given currentGlobalTime, we still need "multiple" iterations
+			}
+		} //end of while (tryNextPhase)
+	}    //end of the method triggerCycleMethods()
+
+	// --------------------------------------------------
+	// the phase_methods
+
+	virtual void startG1Phase(void) {}
+	virtual void runG1Phase(const float) {}
+	virtual void closeG1Phase(void) {}
+
+	virtual void startSPhase(void) {}
+	virtual void runSPhase(const float) {}
+	virtual void closeSPhase(void) {}
+
+	virtual void startG2Phase(void) {}
+	virtual void runG2Phase(const float) {}
+	virtual void closeG2Phase(void) {}
+
+	virtual void startProphase(void) {}
+	virtual void runProphase(const float) {}
+	virtual void closeProphase(void) {}
+
+	virtual void startMetaphase(void) {}
+	virtual void runMetaphase(const float) {}
+	virtual void closeMetaphase(void) {}
+
+	virtual void startAnaphase(void) {}
+	virtual void runAnaphase(const float) {}
+	virtual void closeAnaphase(void) {}
+
+	virtual void startTelophase(void) {}
+	virtual void runTelophase(const float) {}
+	virtual void closeTelophase(void) {}
+
+	virtual void startCytokinesis(void) {}
+	virtual void runCytokinesis(const float) {}
+	virtual void closeCytokinesis(void) {}
 };
 #endif
