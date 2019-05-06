5bc37c976b5895ea03f28acf178357ff4a412047
Vladimír Ulman <ulman@mpi-cbg.de>
2019-05-06 10:55:46 +0200

    ADD: __SHORTFILE__ to the reports -> e.g. "Simulation.h::execute(): some msg"

--- DrosoGen/src/util/report.h
+++ DrosoGen/src/util/report.h
@@ -3,9 +3,12 @@
 
 #include <iostream>
 
+/** this macro is essentially what `basename __FILE__` would do on UNIX prompt/console */
+#define __SHORTFILE__ (strrchr(__FILE__,'/') ? strrchr(__FILE__,'/')+1 : __FILE__)
+
 /** helper macros to unify reports: */
-#define REPORT(x)        std::cout << __FUNCTION__ << "(): " << x << std::endl;
-#define REPORT_NOENDL(x) std::cout << __FUNCTION__ << "(): " << x;
+#define REPORT(x)        std::cout << __SHORTFILE__ << "::" << __FUNCTION__ << "(): " << x << std::endl;
+#define REPORT_NOENDL(x) std::cout << __SHORTFILE__ << "::" << __FUNCTION__ << "(): " << x;
 
 #ifdef DEBUG
 	#define DEBUG_REPORT(x)         REPORT(x)
@@ -16,7 +19,7 @@
 #endif
 
 /** to be used in constructs such as:  throw new std::runtime_error( EREPORT("refuse to deal with NULL agent.") ); */
-#define EREPORT(x) std::string(__FUNCTION__).append("(): ").append(x)
+#define EREPORT(x) std::string(__SHORTFILE__).append("::").append(__FUNCTION__).append("(): ").append(x)
 
 /** shortcut for the often-used std::runtime_error exception:  throw ERROR_REPORT("refuse to deal with NULL agent."); */
 #define ERROR_REPORT(x)  new std::runtime_error( EREPORT(x) )
