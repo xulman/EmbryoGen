e246dcf1438e0e23ad63356c6695449424243c7b
Vladimír Ulman <ulman@mpi-cbg.de>
2019-05-07 21:19:14 +0200

    ADD,WIP: (raw) files from MitoGen and FiloGen

---
+++ DrosoGen/src/util/texture/myround.h
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------
+
+myround.h
+
+This file is part of CytoPacq
+
+Copyright (C) 2007-2013 -- Centre for Biomedical Image Analysis (CBIA)
+
+CytoPacq is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CytoPacq is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CytoPacq. If not, see <http://www.gnu.org/licenses/>.
+
+Author: Martin Maska, David Svoboda
+
+Description: Rounding routines.
+
+-------------------------------------------------------------------------*/
+
+#ifndef _MYROUND_
+#define _MYROUND_
+
+/****************************************************************************/
+
+template <class T> void RoundFloat(float value, T &result);
+
+/****************************************************************************/
+
+#endif
