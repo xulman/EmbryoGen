4e518a4fa02f6662437654735f0d7f880015e9c6
Vladimír Ulman <ulman@mpi-cbg.de>
2019-05-09 11:38:53 +0200

    ADD: TextureQuantized class with its own RenderIntoPhantom(),      also easier isInImage test in Texture*::RenderIntoPhantom()

--- DrosoGen/src/Agents/util/Texture.cpp
+++ DrosoGen/src/Agents/util/Texture.cpp
@@ -49,7 +49,14 @@ void Texture::RenderIntoPhantom(i3d::Image3d<float> &phantoms)
 		dot.pos.toPixels(imgPos, res,off);
 
 		// is the pixel inside the input image?
-		if (phantoms.Include((int)imgPos.x,(int)imgPos.y,(int)imgPos.z))
+		//stdandard way:
+		//if (phantoms.Include((int)imgPos.x,(int)imgPos.y,(int)imgPos.z))
+		//
+		//or, lower bound (but for _unsigned_ makes no sense):
+		//if (imgPos.elemIsPredicateTrue([](const size_t x){ return x >= 0; })
+		//
+		//plus upper bound (tests also underflows... "negative" coordinates)
+		if (imgPos.elemIsLessThan(imgSize))
 		{
 			const float fval = GetBleachFactor(dot.cntOfExcitations);
 #ifdef DEBUG
@@ -69,5 +76,99 @@ void Texture::RenderIntoPhantom(i3d::Image3d<float> &phantoms)
 	REPORT("added total cell intensity: " << meanIntContribution <<
 	       " with mean dot intensity: " << meanIntContribution/(double)meanIntContCounter);
 	REPORT("rendering dots took " << toc(stopWatch));
+#endif
+}
+
+
+void TextureQuantized::RenderIntoPhantom(i3d::Image3d<float> &phantoms)
+{
+	DEBUG_REPORT("going to render " << dots.size() << " quantum dots");
+
+	//cache some output image params
+	const Vector3d<float>  res(phantoms.GetResolution().GetRes());     //in px/um
+	const Vector3d<float>  off(phantoms.GetOffset());                  //in um
+
+	//sweeping-related vars
+	const int xLine   = (signed)phantoms.GetSizeX();                   //in px
+	const int xyPlane = (signed)phantoms.GetSizeY() *xLine;            //in px
+	const Vector3d<float> boxStep(
+		Vector3d<float>( boxSize.x/(float)qCounts.x,
+		                 boxSize.y/(float)qCounts.y,
+		                 boxSize.z/(float)qCounts.z ).elemMult(res) );  //in px
+	Vector3d<float> imgPos;                                            //in px
+
+	//image boundaries after "applying retention zone"
+	const Vector3d<float> halfBoxSize( (0.5f * boxSize).elemMult(res) ); //also a lower bound, px
+	const Vector3d<float> imgSize(                                       //       upper bound, px
+		Vector3d<size_t>(phantoms.GetSize()).to<float>().elemMult(res) - halfBoxSize );
+
+#ifdef DEBUG
+	auto stopWatch = tic();
+
+	// a variable for debugging the photobleaching
+	double meanIntContribution = 0.0f;
+	const float quantumElems = qCounts.x * qCounts.y * qCounts.z;
+	long meanIntContCounter = 0;
+#endif
+
+	float* const paddr = phantoms.GetFirstVoxelAddr();
+	for (auto& dot : dots)
+	{
+		//note that this dot is yet again excited to give some light
+		++(dot.cntOfExcitations);
+
+		// turn micron position to phantom pixel one
+		imgPos.from(dot.pos).toPixels(res,off);
+
+		//test against image boundaries now and skip this dot if it does not
+		//fall _completely_ inside the phantom image; otherwise we would need
+		//to conduct such test with every single quantum -> very slow; this
+		//does not hurt since the occurrence of the agent near (essentially
+		//touching) the boundary should be rather unlikely
+		if (imgPos.elemIsGreaterOrEqualThan(halfBoxSize)
+		 && imgPos.elemIsLessThan(imgSize))
+		{
+			const float fval = GetBleachFactor(dot.cntOfExcitations);
+#ifdef DEBUG
+			meanIntContribution += quantumElems * fval;
+			++meanIntContCounter;
+#endif
+			//std::cout << "--- centre " << imgPos << " um\n";
+
+			//iterate the quantum box from this (float) voxel position
+			imgPos -= halfBoxSize;
+
+			// populate the "quantum cube":
+			for (short iz=0; iz < qCounts.z; ++iz)
+			{
+				//NB: +0.5 is to achieve proper-rounding when casting to int does down-rounding
+				const int Z = int(imgPos.z + (float)iz*boxStep.z +0.5f);
+
+				for (short iy=0; iy < qCounts.y; ++iy)
+				{
+					const int Y = int(imgPos.y + (float)iy*boxStep.y +0.5f);
+
+					float* const T = paddr + Z*xyPlane + Y*xLine;
+					for (short ix=0; ix < qCounts.x; ++ix)
+					{
+						const int X = int(imgPos.x + (float)ix*boxStep.x +0.5f);
+						T[X] += fval;;
+
+						//std::cout << "populating at [" << X << "," << Y << "," << Z << "] px because "
+						//          << ( imgPos+Vector3d<float>(ix,iy,iz).elemMult(boxStep) ) << " um\n";
+					}
+				}
+			}
+		}
+		else
+		{
+			DEBUG_REPORT("ups, dot " << dot.pos << " is outside the phantom image");
+		}
+	}
+
+#ifdef DEBUG
+	REPORT("added total cell intensity: " << meanIntContribution <<
+	       " with mean dot intensity: " << meanIntContribution/(double)meanIntContCounter);
+	REPORT("rendering quantum dots took " << toc(stopWatch));
 #endif
 }
--- DrosoGen/src/Agents/util/Texture.h
+++ DrosoGen/src/Agents/util/Texture.h
@@ -2,7 +2,9 @@
 #define TEXTURE_H
 
 #include <vector>
+#include <cmath>
 #include <i3d/image3d.h>
+#include "../../util/report.h"
 #include "../../util/rnd_generators.h"
 #include "../../util/Dots.h"
 
@@ -61,6 +63,82 @@ public:
 	// rendering
 
 	/** renders the current content of the this->dots list into the given phantom image */
+	void RenderIntoPhantom(i3d::Image3d<float> &phantoms);
+};
+
+/**
+ * Utility class that maintains texture particles, e.g. fluorescence dots, that assumes
+ * that one such particle represents a 'quantum' amount of (equally exposed/bleached)
+ * dots. The quantum is defined as a number of fluorescence molecules equally spread
+ * throughout given (axis aligned) box. Often, one chooses the box size to correspond
+ * with the volume of one voxel in the phantom image. Furthermore, the number of the
+ * fl. molecules (the quantum size) is a compromise to obtain phantom images with
+ * desired intensity levels while maintaining computational tractability (that is,
+ * not having exceedingly long list with (small) quanta).
+ */
+class TextureQuantized : public Texture
+{
+public:
+	/** c'tor that requires explicitly the number of fl. molecules per volumetric box */
+	TextureQuantized(const unsigned int expectedNoOfQuantumDots,
+	                 const Vector3d<float>& _boxSize,
+	                 const Vector3d<short>& _qCounts):
+		Texture(expectedNoOfQuantumDots),
+		boxSize(_boxSize), qCounts(_qCounts)
+	{
+		checkCountsPerAxis();
+	}
+
+	/** a utility c'tor that estimates the quantum box from voxel size and, essentially,
+	    a phantom image intensity increase/gain */
+	TextureQuantized(const unsigned int expectedNoOfQuantumDots,
+	                 const Vector3d<float>& imgRes,
+	                 const short quantumGain):
+		Texture(expectedNoOfQuantumDots),
+		boxSize(1.0f/imgRes.x, 1.0f/imgRes.y, 1.0f/imgRes.z),
+		qCounts( getCountsPerAxis(imgRes,quantumGain) )
+	{
+		DEBUG_REPORT("Converting inputs to: boxSize=" << boxSize <<", qCounts=" << qCounts);
+		checkCountsPerAxis();
+	}
+
+	Vector3d<short> getCountsPerAxis(const Vector3d<float>& imgRes, const short quantumGain) const
+	{
+		//dots per voxel
+		float quantum = quantumGain;
+
+		//density in dots/um^3
+		quantum *= imgRes.x*imgRes.y*imgRes.z;
+
+		//dots (density) per one axis
+		quantum = std::cbrt(quantum);
+
+		//how many dots per axis, honours anisotropy (if present)
+		Vector3d<short> counts;
+		counts.x = (short)std::ceil(quantum * boxSize.x);
+		counts.y = (short)std::ceil(quantum * boxSize.y);
+		counts.z = (short)std::ceil(quantum * boxSize.z);
+		//should hold x*y*z >= original input quantum, ideally closest possible to the equality
+
+		return counts;
+	}
+
+	void checkCountsPerAxis() const
+	{
+		if (! qCounts.elemIsGreaterThan( Vector3d<short>(0) ) )
+			throw ERROR_REPORT("Quantum cube must have at least one dot along every axis");
+	}
+
+	/** defines (axis-aligned) box to represent a volume of the one quantum box */
+	const Vector3d<float> boxSize;
+
+	/** counts of quantum dots along every axis, multiplying the .x, .y and .z gives
+	    the total contribution of this one quantum box */
+	const Vector3d<short> qCounts;
+
+	/** renders (quantum-wise) the current content of the this->dots list into the given phantom image,
+	    the contributed intensity to the image should be 'quantum'-times greater than what would provide
+		 the upstream, non-quantum Texture::RenderIntoPhantom() */
 	void RenderIntoPhantom(i3d::Image3d<float> &phantoms);
 };
 #endif
