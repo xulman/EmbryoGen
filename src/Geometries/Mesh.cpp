8431b93fd09ebe4b811ecdf31963698aad1ee269
Vladimír Ulman <ulman@mpi-cbg.de>
2019-12-03 16:11:38 +0100

    All 4 Geometries

---
+++ src/Geometries/Mesh.cpp
@@ -0,0 +1,39 @@
+#include "../util/report.h"
+#include "Mesh.h"
+
+/** calculate min surface distance between myself and some foreign agent */
+void Mesh::getDistance(const Geometry& otherGeometry,
+                       std::list<ProximityPair>& l) const
+{
+	switch (otherGeometry.shapeForm)
+	{
+	case ListOfShapeForms::Spheres:
+		//TODO: attempt to project mesh vertices against the spheres (or their AABB) and look for collision
+		REPORT("this.Mesh vs Spheres is not implemented yet!");
+		break;
+	case ListOfShapeForms::Mesh:
+		//TODO identity case
+		REPORT("this.Mesh vs Mesh is not implemented yet!");
+		break;
+	case ListOfShapeForms::ScalarImg:
+	case ListOfShapeForms::VectorImg:
+		//find collision "from the other side"
+		getSymmetricDistance(otherGeometry,l);
+		break;
+
+	case ListOfShapeForms::undefGeometry:
+		REPORT("Ignoring other geometry of type 'undefGeometry'.");
+		break;
+	default:
+		throw ERROR_REPORT("Not supported combination of shape representations.");
+	}
+}
+
+
+/** construct AABB from the given Mesh */
+void Mesh::updateThisAABB(AxisAlignedBoundingBox& /* AABB */) const
+{
+	//scan through the mesh vertices/nodes and find extremal coordinates
+	//TODO
+	REPORT("not implemented yet!");
+}
--- src/Geometries/VectorImg.h
+++ src/Geometries/VectorImg.h
@@ -2,11 +2,10 @@
 #define VECTORIMG_H
 
 #include <i3d/image3d.h>
-#include <i3d/DistanceTransform.h>
 #include "../util/report.h"
 #include "../util/FlowField.h"
 #include "Geometry.h"
-#include "Spheres.h"
+class Spheres;
 
 /**
  * The class essentially represents a 3D vector field that spans a certain volume.
@@ -133,41 +132,14 @@ public:
 			++m; ++x; ++y; ++z;
 		}
 
-		mImg.SaveImage(filename);
+		mImg.SaveImage(fullFilename);
 	}
 
 
 	// ------------- distances -------------
 	/** calculate min surface distance between myself and some foreign agent */
 	void getDistance(const Geometry& otherGeometry,
-	                 std::list<ProximityPair>& l) const override
-	{
-		switch (otherGeometry.shapeForm)
-		{
-		case ListOfShapeForms::Spheres:
-			getDistanceToSpheres((class Spheres*)&otherGeometry,l);
-			break;
-		case ListOfShapeForms::Mesh:
-			//TODO: attempt to project mesh vertices into the mask image and look for collision
-			REPORT("this.VectorImg vs Mesh is not implemented yet!");
-			break;
-		case ListOfShapeForms::ScalarImg:
-			//find collision "from the other side"
-			getSymmetricDistance(otherGeometry,l);
-			break;
-		case ListOfShapeForms::VectorImg:
-			//TODO identity case
-			REPORT("this.VectorImg vs VectorImg is not implemented yet!");
-			//getDistanceToVectorImg((VectorImg*)&otherGeometry,l);
-			break;
-
-		case ListOfShapeForms::undefGeometry:
-			REPORT("Ignoring other geometry of type 'undefGeometry'.");
-			break;
-		default:
-			throw ERROR_REPORT("Not supported combination of shape representations.");
-		}
-	}
+	                 std::list<ProximityPair>& l) const override;
 
 	/** Specialized implementation of getDistance() for VectorImg & Spheres geometries.
 	    Rasterizes the 'other' spheres into the 'local' VectorImg and, based on this->policy
@@ -188,215 +160,12 @@ public:
 	    one's own needs (in this light this function is more a convenience function than
 	    anything else). */
 	void getDistanceToSpheres(const class Spheres* otherSpheres,
-	                          std::list<ProximityPair>& l) const
-	{
-		//da plan: determine bounding box within this VectorImg where
-		//we can potentially see any piece of the foreign Spheres;
-		//sweep it and consider voxel centres; construct a thought
-		//vector from a sphere's centre to the voxel centre, make it
-		//that sphere's radius long and check the end of this vector
-		//(when positioned at sphere's centre) if it falls into the
-		//currently examined voxel; if it does, we have found a voxel
-		//that contains a piece of the sphere's surface
-		//
-		//if half of a voxel diagonal is pre-calculated, one can optimize
-		//by first comparing it against abs.val. of the difference of
-		//the sphere's radius from the length of the thought vector
-
-		//sweeping box in micrometers, initiated to match spheres' bounding box
-		AxisAlignedBoundingBox sweepBox(otherSpheres->AABB);
-
-		//intersect the sweeping box with this AABB
-		//(as only this AABB wraps around interesting information in the mask image,
-		// the image might be larger (especially in the GradIN_ZeroOUT model))
-		sweepBox.minCorner.elemMax(AABB.minCorner); //in mu
-		sweepBox.maxCorner.elemMin(AABB.maxCorner);
-
-		//the sweeping box in pixels, in coordinates of this FF
-		Vector3d<size_t> curPos, minSweepPX,maxSweepPX;
-		sweepBox.exportInPixelCoords(X, minSweepPX,maxSweepPX);
-
-		//(squared) voxel's volume half-diagonal vector and its length
-		//(for detection of voxels that coincide with sphere's surface)
-		Vector3d<FLOAT> vecPXhd2(0.5f/imgRes.x,0.5f/imgRes.y,0.5f/imgRes.z);
-		const FLOAT     lenPXhd = vecPXhd2.len();
-		vecPXhd2.elemMult(vecPXhd2); //make it squared
-
-		//shortcuts to the otherGeometry's spheres
-		const Vector3d<FLOAT>* const centresO = otherSpheres->getCentres();
-		const FLOAT* const radiiO             = otherSpheres->getRadii();
-		const int io                          = otherSpheres->getNoOfSpheres();
-
-		//remember the squared lengths observed so far for every foreign sphere...
-		FLOAT* lengths2 = new FLOAT[io];
-		switch (policy)
-		{ //initialize the array
-		case minVec:
-			for (int i = 0; i < io; ++i) lengths2[i] = TOOFAR;
-			break;
-		case maxVec:
-		case avgVec:
-			for (int i = 0; i < io; ++i) lengths2[i] = 0;
-			break;
-		case allVec:
-		default: ;
-			//no init required
-		}
-		//...what was observed...
-		Vector3d<FLOAT>* vecs = new Vector3d<FLOAT>[io]; //inits to zero vector by default
-		//...and where it was observed
-		Vector3d<size_t>* hints = new Vector3d<size_t>[io];
-
-		//holder for the currently examined vectors in the FF
-		Vector3d<FLOAT> vec;
-
-		//aux position vectors: current voxel's centre and somewhat sphere's surface point
-		Vector3d<FLOAT> centre, surfPoint;
-
-		//finally, sweep and check intersection with spheres' surfaces
-		for (curPos.z = minSweepPX.z; curPos.z < maxSweepPX.z; curPos.z++)
-		for (curPos.y = minSweepPX.y; curPos.y < maxSweepPX.y; curPos.y++)
-		for (curPos.x = minSweepPX.x; curPos.x < maxSweepPX.x; curPos.x++)
-		{
-			//we are now visiting voxels where some sphere can be seen,
-			//get micron coordinate of the current voxel's centre
-			centre.toMicronsFrom(curPos, imgRes,imgOff);
-
-			//check the current voxel against all spheres
-			for (int i = 0; i < io; ++i)
-			{
-				surfPoint  = centre;
-				surfPoint -= centresO[i];
-				//if sphere's surface would be 2*lenPXhd thick, would the voxel's center be in?
-				if (std::abs(surfPoint.len() - radiiO[i]) < lenPXhd)
-				{
-					//found a voxel _nearby_ i-th sphere's true surface,
-					//is there really an intersection?
-
-					//a vector pointing from voxel's centre to the nearest sphere surface
-					surfPoint *= radiiO[i]/surfPoint.len() -1.0f;
-					//NB: surfPoint *= (radiiO[i] - surfPoint.len()) / surfPoint.len()
-
-					//max (squared; only to avoid using std::abs()) distance to the voxel's centre
-					//to see if surfPoint's tip is within this voxel
-					surfPoint.elemMult(surfPoint);
-					if (surfPoint.x <= vecPXhd2.x && surfPoint.y <= vecPXhd2.y && surfPoint.z <= vecPXhd2.z)
-					{
-						//hooray, a voxel whose volume is intersecting with i-th sphere's surface
-						//let's inspect the FF's vector at this position
-						vec.x = X.GetVoxel(curPos.x,curPos.y,curPos.z);
-						vec.y = Y.GetVoxel(curPos.x,curPos.y,curPos.z);
-						vec.z = Z.GetVoxel(curPos.x,curPos.y,curPos.z);
-
-						switch (policy)
-						{
-						case minVec:
-							if (vec.len2() < lengths2[i])
-							{
-								lengths2[i] = vec.len2();
-								vecs[i] = vec;
-								hints[i] = curPos;
-							}
-							break;
-						case maxVec:
-							if (vec.len2() > lengths2[i])
-							{
-								lengths2[i] = vec.len2();
-								vecs[i] = vec;
-								hints[i] = curPos;
-							}
-							break;
-						case avgVec:
-							vecs[i] += vec;
-							lengths2[i] += 1; //act as count here
-							break;
-						case allVec:
-							//exact point on the sphere's surface
-							surfPoint  = centre;
-							surfPoint -= centresO[i];
-							surfPoint *= radiiO[i]/surfPoint.len();
-							surfPoint += centresO[i];
-
-							//create immediately the ProximityPair
-							l.emplace_back( surfPoint+vec,surfPoint, vec.len(),
-							  (signed)X.GetIndex(curPos.x,curPos.y,curPos.z),i );
-							break;
-						default: ;
-						}
-
-					} //if intersecting voxel was found
-				} //if nearby voxel was found
-			} //over all foreign spheres
-		} //over all voxels in the sweeping box
-
-		//process (potentially) the one vector per sphere
-		if (policy == minVec)
-		{
-			//add ProximityPairs where found some
-			for (int i = 0; i < io; ++i)
-			if (lengths2[i] < TOOFAR)
-			{
-				//found one
-				//calculate exact point on the sphere's surface (using hints[i])
-				surfPoint.toMicronsFrom(hints[i], imgRes,imgOff);
-				surfPoint -= centresO[i];
-				surfPoint *= radiiO[i]/surfPoint.len();
-				surfPoint += centresO[i];
-
-				//this is from VectorImg perspective (local = VectorImg, other = Sphere),
-				//it reports index of the relevant foreign sphere
-				l.emplace_back( surfPoint+vecs[i],surfPoint, std::sqrt(lengths2[i]),
-				  (signed)X.GetIndex(hints[i].x,hints[i].y,hints[i].z),i );
-			}
-		}
-		else if (policy == maxVec)
-		{
-			//add ProximityPairs where found some
-			for (int i = 0; i < io; ++i)
-			if (lengths2[i] > 0)
-			{
-				//found one
-				//calculate exact point on the sphere's surface (using hints[i])
-				surfPoint.toMicronsFrom(hints[i], imgRes,imgOff);
-				surfPoint -= centresO[i];
-				surfPoint *= radiiO[i]/surfPoint.len();
-				surfPoint += centresO[i];
-
-				//this is from VectorImg perspective (local = VectorImg, other = Sphere),
-				//it reports index of the relevant foreign sphere
-				l.emplace_back( surfPoint+vecs[i],surfPoint, std::sqrt(lengths2[i]),
-				  (signed)X.GetIndex(hints[i].x,hints[i].y,hints[i].z),i );
-			}
-		}
-		else if (policy == avgVec)
-		{
-			//add ProximityPairs where found some
-			for (int i = 0; i < io; ++i)
-			if (lengths2[i] > 0)
-			{
-				//found at least one
-				vecs[i] /= lengths2[i];
-
-				//this is from VectorImg perspective (local = VectorImg, other = Sphere),
-				//it reports index of the relevant foreign sphere
-				l.emplace_back( centresO[i]+vecs[i],centresO[i], vecs[i].len(), 0,i );
-				//NB: average vector is no particular existing vector, hence local hint = 0,
-				//    and placed in the sphere's centre
-			}
-		}
-		//else allVec -- this has been already done above
-
-		delete[] lengths2;
-		delete[] vecs;
-		delete[] hints;
-	}
+	                          std::list<ProximityPair>& l) const;
 
 	/** Specialized implementation of getDistance() for VectorImg-VectorImg geometries. */
 	/*
 	void getDistanceToVectorImg(const VectorImg* otherVectorImg,
-	                            std::list<ProximityPair>& l) const
-	{
-	}
+	                            std::list<ProximityPair>& l) const;
 	*/
 
 
@@ -404,12 +173,7 @@ public:
 	/** construct AABB from the the mask image considering
 	    only non-zero valued voxels, and considering mask's
 	    offset and resolution */
-	void updateThisAABB(AxisAlignedBoundingBox& AABB) const override
-	{
-		//the AABB is the whole image
-		AABB.minCorner = imgOff;
-		AABB.maxCorner = imgFarEnd;
-	}
+	void updateThisAABB(AxisAlignedBoundingBox& AABB) const override;
 
 
 	// ------------- get/set methods -------------
