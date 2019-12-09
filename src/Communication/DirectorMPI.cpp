fe384fee801ba65c45aa6e73df93b078037ffbd4
Vladimír Ulman <ulman@mpi-cbg.de>
2019-12-09 13:10:28 +0100

    ADD: skeleton (empty) methods in Communication/*MPI.cpp files

--- src/Communication/DirectorMPI.cpp
+++ src/Communication/DirectorMPI.cpp
@@ -1,7 +1,166 @@
 #include "../Director.h"
+#include "../FrontOfficer.h"
 
-void Director::trigger_publishGeometry()
+//in the SMP world, a lot of respond_...() methods are actually
+//empty because the FOs reach directly the implementing code
+
+void Director::respond_getNextAvailAgentID()
+{
+	//MPI world:
+
+	//gets : nothing
+	//gives: int
+
+	/*
+	int sendBackThisNewID = getNextAvailAgentID();
+	*/
+}
+
+
+void Director::respond_startNewAgent()
+{
+	//MPI world:
+
+	//gets : int, int, bool
+	//gives: nothing
+
+	//have to learn from the other party:
+	/*
+	int newAgentID;
+	int associatedFO;
+	bool wantsToAppearInCTCtracksTXTfile;
+
+	startNewAgent(newAgentID,associatedFO,wantsToAppearInCTCtracksTXTfile);
+	*/
+}
+
+
+void Director::respond_closeAgent()
+{
+	//MPI world:
+
+	//gets : int, int
+	//gives: nothing
+
+	//have to learn from the other party:
+	/*
+	int agentID;
+	int associatedFO;
+
+	closeAgent(agentID,associatedFO);
+	*/
+}
+
+
+void Director::respond_updateParentalLink()
+{
+	//MPI world:
+
+	//gets : int, int
+	//gives: nothing
+
+	//have to learn from the other party:
+	/*
+	int childID;
+	int parentID;
+
+	startNewDaughterAgent(childID,parentID);
+	*/
+}
+
+
+void Director::notify_publishAgentsAABBs(const int /* FOsID */)
+{
+	//notify the first FO
+	//MPI_signalThisEvent_to( FOsID );
+}
+
+
+void Director::waitFor_publishAgentsAABBs()
+{
+	//wait here until we're told (that the broadcasting is over)
+
+	//in MPI case, really do wait for the event to come
+}
+
+
+void Director::respond_AABBofAgent()
+{
+	//we ignore these notifications entirely
+	//
+	//perhaps we would see a reason later why Direktor would need
+	//to know spatial relation among all agents
+
+	//gets : AABB+type as 6x float, int, string
+	//gives: nothing
+}
+
+
+size_t Director::request_CntOfAABBs(const int /* FOsID */)
+{
+	//block and wait until size_t comes back from the front officer FOsID
+	return 10; //faked value!
+}
+
+
+void Director::notify_setDetailedDrawingMode(const int /* FOsID */, const int agentID, const bool state)
+{
+	//in the SMP world:
+	//FO->setAgentsDetailedDrawingMode(agentID,state);
+	//
+	//in the MPI, send the above to the front officer FOsID
+}
+
+
+void Director::notify_setDetailedReportingMode(const int /* FOsID */, const int agentID, const bool state)
+{
+	//in the SMP world:
+	//FO->setAgentsDetailedReportingMode(agentID,state);
+	//
+	//in the MPI, send the above to the agent FOsID
+}
+
+
+void Director::waitHereUntilEveryoneIsHereToo()
 {}
 
-void Director::trigger_renderNextFrame()
-{}
+
+void Director::request_renderNextFrame(const int /* FOsID */)
+{
+	//in the SMP:
+	//FO->renderNextFrame();
+
+	//MPI world:
+	//this exactly the same code as in FrontOfficer::request_renderNextFrame(FOsID)
+}
+
+
+void Director::waitFor_renderNextFrame()
+{
+	//MPI world:
+	//this exactly the same code as in FrontOfficer::waitFor_renderNextFrame()
+}
+
+
+void Director::broadcast_setRenderingDebug(const bool setFlagToThis)
+{
+	//SMP:
+	//FO->setSimulationDebugRendering(setFlagToThis);
+
+	//MPI world:
+	//non-blocking send out to everyone
+}
+
+
+void Director::broadcast_throwException(const char* /* exceptionMessage */)
+{
+	//MPI world:
+	//the same as FrontOfficer::broadcast_throwException(exceptionMessage)
+}
+
+
+void Director::respond_throwException()
+{
+	//MPI world:
+	//the same as FrontOfficer::respond_throwException()
+}
--- src/Communication/FrontOfficerMPI.cpp
+++ src/Communication/FrontOfficerMPI.cpp
@@ -1,12 +1,289 @@
+#include "../Agents/AbstractAgent.h"
 #include "../FrontOfficer.h"
+#include "../Director.h"
 
-void FrontOfficer::triggered_publishGeometry()
+int FrontOfficer::request_getNextAvailAgentID()
 {
-	//round robin calls to all my agents and their publishGeometry()
+	//return Direktor->getNextAvailAgentID();
+	//blocks and waits until it gets int back from the Director
+	return 1;
 }
-void FrontOfficer::triggered_renderNextFrame()
+
+
+void FrontOfficer::request_startNewAgent(const int newAgentID,
+                                         const int associatedFO,
+                                         const bool wantsToAppearInCTCtracksTXTfile)
 {
-	//shall obtain images to render into
-	//round robin calls to all my agents and their draw*()
-	//shall use: nextFOsID
+	//Direktor->startNewAgent(newAgentID,associatedFO,wantsToAppearInCTCtracksTXTfile);
+	//may block and wait until Direktor confirms the request was processed
+}
+
+
+void FrontOfficer::request_closeAgent(const int agentID,
+                                      const int associatedFO)
+{
+	//Direktor->closeAgent(agentID,associatedFO);
+	//may block and wait until Direktor confirms the request was processed
+}
+
+
+void FrontOfficer::request_updateParentalLink(const int childID, const int parentID)
+{
+	//Direktor->startNewDaughterAgent(childID,parentID);
+	//may block and wait until Direktor confirms the request was processed
+}
+
+
+void FrontOfficer::waitFor_publishAgentsAABBs()
+{
+	//wait here until we're told to start the broadcast which
+	//our agents were removed and AABBs of the remaining ones
+
+	//in MPI case, really do wait for the event to come
+}
+
+void FrontOfficer::notify_publishAgentsAABBs(const int /* FOsID */)
+{
+	//once our broadcasting is over, notify next FO to do the same
+	//(or notify the Direktor if we're the last on the round-robin-chain)
+
+	//in this SMP particular implementation we do nothing because
+	//there is no other FO, and the Direktor called us directly
+
+	//in MPI case, tell the next FO (or the Direktor)
+	//MPI_signalThisEvent_to( FOsID );
+}
+
+
+void FrontOfficer::broadcast_AABBofAgent(const ShadowAgent& ag)
+{
+	//this shall tell all (including this one) FOs the AABB of the given agent,
+	//the Direktor actually does not care
+
+	/*
+	//in MPI world: example:
+	float coords[] = {ag.getAABB().minCorner.x,
+	                  ag.getAABB().minCorner.y,
+	                  ag.getAABB().minCorner.z,
+	                  ag.getAABB().maxCorner.x,
+	                  ag.getAABB().maxCorner.y,
+	                  ag.getAABB().maxCorner.z};
+	//important, also transfer agent's ID and type/string:
+	ag.getID();
+	ag.getAgentType();
+	int sendVersion = ag.getGeometry().version;
+	//send out 6x float, int, string, int --> all to be received by respond_AABBofAgent()
+
+	//TODO: check that the broadcast reaches myself too!
+	//      or just update myself explicitly just like above in the SMP case
+	*/
+}
+
+
+void FrontOfficer::respond_AABBofAgent()
+{
+	//MPI world:
+
+	//gets : AABB +ID +type +geomVersion as 6x float, int, string, int
+	//gives: nothing
+	//also needs to get the ID of the FO that broadcasted that particular message
+	//(if that is not possible, the sending FOsID will need be part of the message)
+
+	/*
+	//fake example values:
+	int FOsIDfromWhichTheMessageArrived(1);
+	//
+	float coords[] = { 10.f,10.f,10.f, 20.f,20.f,20.f };
+	int         agentID(10);
+	std::string agentType("some fictious agent");
+	int         geomVersion(42);
+
+	AABBs.emplace_back();
+	AABBs.back().minCorner.fromScalars(coords[0],coords[1],coords[2]);
+	AABBs.back().maxCorner.fromScalars(coords[3],coords[4],coords[5]);
+	AABBs.back().ID   = agentID;
+	AABBs.back().name = agentType;
+	agentsAndBroadcastGeomVersions[agentID] = geomVersion;
+	registerThatThisAgentIsAtThisFO(agentID,FOsIDfromWhichTheMessageArrived);
+	*/
+}
+
+
+void FrontOfficer::respond_CntOfAABBs()
+{
+	//MPI world:
+
+	//gets : nothing
+	//gives: int
+
+	/*
+	size_t sendBackMyCount = getSizeOfAABBsList();
+	*/
+}
+
+
+ShadowAgent* FrontOfficer::request_ShadowAgentCopy(const int /* agentID */, const int /* FOsID */)
+{
+	//faked return!
+	return NULL;
+
+	//MPI world:
+	//contacts FO at FOsID telling it that it needs agentID's ShadowAgent back
+
+	//gives: agentID, this->ID (to tell the other party whom to talk back)
+	//waits
+	//gets : Geometry data, agentID and agentType
+
+	/* fake data!
+	Geometry*   gotThisGeom      = new Spheres(4);
+	int         gotThisAgentID   = 10;
+	std::string gotThisAgentType = "fake agent";
+	return new ShadowAgent(*gotThisGeom, gotThisAgentID,gotThisAgentType);
+	*/
+}
+
+
+void FrontOfficer::respond_ShadowAgentCopy()
+{
+	//MPI world:
+
+	//gets : requestedAgentID, askingFOsID
+	//gives: Geometry data, agentID and agentType
+
+	/*
+	int requestedAgentID = 10; //fake value
+
+#ifdef DEBUG
+	if (agents.find(requestedAgentID) == agents.end())
+		throw ERROR_REPORT("Cannot provide ShadowAgent for agent ID " << requestedAgentID);
+#endif
+
+	const AbstractAgent& aaRef = *(agents[requestedAgentID]);
+
+	const Geometry&    sendBackGeom      = aaRef.getGeometry();
+	const int          sendBackAgentID   = aaRef.getID();
+	const std::string& sendBackAgentType = aaRef.getAgentType();
+	*/
+}
+
+
+void FrontOfficer::respond_setDetailedDrawingMode()
+{
+	//MPI world:
+
+	//gets : int, bool
+	//gives: nothing
+
+	/*
+	int agentID = 1;
+	bool state = false;
+	setAgentsDetailedDrawingMode(agentID,state);
+	*/
+}
+
+
+void FrontOfficer::respond_setDetailedReportingMode()
+{
+	//MPI world:
+
+	//gets : int, bool
+	//gives: nothing
+
+	/*
+	int agentID = 1;
+	bool state = false;
+	setAgentsDetailedReportingMode(agentID,state);
+	*/
+}
+
+
+void FrontOfficer::waitHereUntilEveryoneIsHereToo()
+{}
+
+
+void FrontOfficer::waitFor_renderNextFrame()
+{
+	//MPI world:
+
+	//shall block and wait until 3 images come to us
+	//they will come in this order: mask, phantom, optics
+
+#ifdef DEBUG
+	//test this:
+	//the incomming images must be of the same size as ours sc.img*
+#endif
+
+	//one receives each image directly into our own sc.img* image,
+	//to "receive directly" is to sum (operation +) together corresponding
+	//pixels, that is pixel from the network with pixel in the sc.img*,
+	//the result is stored into the sc.img*
+
+	/*
+	SceneControls& sc = scenario.params;
+
+	//here's the handle on our sc.img* images
+	sc.imgMask
+	sc.imgPhantom
+	sc.imgOptics
+
+	//here's how to reach the pixels, they live in one long buffer, e.g.
+	i3d::GRAY16* maskPixelBuffer = sc.imgMask.GetFirstVoxelAddr();
+	const size_t maskPixelLength = sc.imgMask.GetImageSize();
+
+	//reminder of the types of the images:
+	i3d::Image3d<i3d::GRAY16> imgMask;
+	i3d::Image3d<float> imgPhantom;
+	i3d::Image3d<float> imgOptics;
+
+	(i3d::GRAY16 maps into short type)
+	*/
+}
+
+
+void FrontOfficer::request_renderNextFrame(const int /* FOsID */)
+{
+	//MPI world:
+
+	//this is the counterpart to the waitFor_renderNextFrame()
+	//one sends out the images in this order: mask, phantom, optics
+}
+
+
+void FrontOfficer::respond_setRenderingDebug()
+{
+	//MPI world:
+
+	//gets : bool
+	//gives: nothing
+
+	/*
+	bool gotThisFlagValue = false; //fake value here!
+	setSimulationDebugRendering(gotThisFlagValue);
+	*/
+}
+
+
+void FrontOfficer::broadcast_throwException(const char* /* exceptionMessage */)
+{
+	//MPI world:
+
+	//gets : nothing
+	//gives: char*
+
+	//just non-blocking broadcast of the exceptionMessage, don't throw
+	//it here because this should only be called already from the catch(),
+	//that is the exception has already been thrown...
+}
+
+
+void FrontOfficer::respond_throwException()
+{
+	//MPI world:
+
+	//gets : bool
+	//gives: nothing
+
+	//read the exceptionMessage, then throw it
+	char msg[] = "received this fake exception message";
+	throw new std::runtime_error( msg );
 }
