9919f4891015ae1e7cccf5156ee676ec07099f86
Vladimir Ulman <ulman@fi.muni.cz>
2020-06-22 11:11:03 +0200

    ADD2/2: script to prepare deps (libs/prepareEverything.sh):
    FIX: libs/prepareEverything.sh pimped to collect all .a files into one folder
    CHG,ADD: libs/prepareEverything adds f2c, .gitignore pimped,
             CMake cleaned, addModulesAtIT4I added
    FIX: libs/prepareEverything prepares i3dlibs against locally prepared libs
    FIX: Convenience preset in CMakeLists in the static leg
    FIX: cleaned CMakeLists from gfortran commented line
    FIX: hardening prepareEverything.sh

--- libs/prepareEverything.sh
+++ libs/prepareEverything.sh
@@ -10,10 +10,15 @@ prepare_LAPACKwithBLAS=YES
 prepare_ZLIB=YES
 prepare_TIFF=YES
 prepare_FFTW=YES
+prepare_F2C=YES
 prepare_I3D=YES
 
 
 mkdir -p $DEPLOYDIR
+
+# this makes sure that all .a files end up in one folder
+mkdir -p $DEPLOYDIR/lib64
+test ! -e $DEPLOYDIR/lib && ln -vs $DEPLOYDIR/lib64  $DEPLOYDIR/lib
 
 echo ========================= ICS =========================
 if [ "_$prepare_ICS" == "_YES" ]; then
@@ -21,7 +26,7 @@ if [ "_$prepare_ICS" == "_YES" ]; then
 	wget https://github.com/svi-opensource/libics/archive/$ICS_archive
 	tar -xzf $ICS_archive
 	cd libics-${ICS_archive:0:5}
-	mkdir BUILD
+	mkdir -p BUILD
 	cd BUILD
 	cmake -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
 	make -j${MAKEJOBS}
@@ -51,7 +56,7 @@ if [ "_$prepare_LAPACKwithBLAS" == "_YES" ]; then
 	wget https://github.com/Reference-LAPACK/lapack-release/archive/$LAPACK_archive
 	unzip $LAPACK_archive
 	cd lapack-release-lapack-${LAPACK_archive:7:5}
-	mkdir BUILD
+	mkdir -p BUILD
 	cd BUILD
 	cmake -D CBLAS=ON -D LAPACKE=ON -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
 	make -j${MAKEJOBS}
@@ -67,7 +72,7 @@ if [ "_$prepare_ZLIB" == "_YES" ]; then
 	wget https://www.zlib.net/$ZLIB_archive
 	tar -xzf $ZLIB_archive
 	cd ${ZLIB_archive:0:11}
-	mkdir BUILD
+	mkdir -p BUILD
 	cd BUILD
 	cmake -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
 	make -j${MAKEJOBS}
@@ -83,7 +88,7 @@ if [ "_$prepare_TIFF" == "_YES" ]; then
 	wget http://download.osgeo.org/libtiff/$TIFF_archive
 	tar -xzf $TIFF_archive
 	cd ${TIFF_archive:0:11}
-	mkdir BUILD
+	mkdir -p BUILD
 	cd BUILD
 	cmake -D BUILD_SHARED_LIBS=OFF -D jbig=OFF -D jpeg=OFF -D jpeg12=OFF -D webp=OFF -D zstd=OFF -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
 	make -j${MAKEJOBS}
@@ -99,7 +104,7 @@ if [ "_$prepare_FFTW" == "_YES" ]; then
 	wget http://www.fftw.org/$FFTW_archive
 	tar -xzf $FFTW_archive
 	cd ${FFTW_archive:0:10}
-	mkdir BUILD
+	mkdir -p BUILD
 	cd BUILD
 	cmake -D BUILD_SHARED_LIBS=OFF -D DISABLE_FORTRAN=ON -D ENABLE_FLOAT=ON  -D ENABLE_LONG_DOUBLE=OFF -D ENABLE_THREADS=ON -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
 	make -j${MAKEJOBS} install
@@ -112,26 +117,48 @@ else
 	echo "not now"
 fi
 
+echo ========================= F2C =========================
+if [ "_$prepare_F2C" == "_YES" ]; then
+	F2C_archive=libf2c.zip
+	wget https://www.netlib.org/f2c/$F2C_archive
+	mkdir -p ${F2C_archive:0:6}
+	cd ${F2C_archive:0:6}
+	unzip ../$F2C_archive
+	cp -i makefile.u makefile
+	make hadd
+	make
+	cp -v libf2c.a $DEPLOYDIR/lib64
+	cd ..
+else
+	echo "not now"
+fi
+
 echo ========================= I3D =========================
 if [ "_$prepare_I3D" == "_YES" ]; then
 	git clone git@gitlab.fi.muni.cz:cbia/I3DLIB.git i3dlibs
 	cd i3dlibs
-	mkdir BUILD
+	mkdir -p BUILD
 	cd BUILD
 	cmake -D ALGO_WITH_BLAS=ON -D ALGO_WITH_LAPACK=ON -D CORE_WITH_BIOFORMATS=OFF -D CORE_WITH_DCM=OFF \
 	      -D CORE_WITH_HDF5=OFF -D CORE_WITH_JPEG=OFF -D CORE_WITH_METAIO=OFF -D CORE_WITH_PNG=OFF \
-	      -D CORE_WITH_TARGA=OFF -D GLOBAL_BUILD_SHARED_LIBS=OFF -D GLOBAL_INSTALL_PATH=$DEPLOYDIR ..
-
-# for some time next... it works overall even without this (that is with i3ds built against .so)
-#	      -D ALGO_FFTW_LIB_REL=../../DEPLOY/lib64/libfftw3.a \
-#	      -D ALGO_FFTW_THREADS_LIB_REL=../../DEPLOY/lib64/libfftw3_threads.a \
-#	      -D ALGO_FFTWF_LIB_REL=../../DEPLOY/lib64/libfftw3f.a \
-#	      -D ALGO_FFTWF_THREADS_LIB_REL=../../DEPLOY/lib64/libfftw3f_threads.a \
-#	      -D ALGO_FFTWL_LIB_REL=../../DEPLOY/lib64/libfftw3l.a \
-#	      -D ALGO_FFTWL_THREADS_LIB_REL=../../DEPLOY/lib64/libfftw3l_threads.a \
-#	      -D CORE_ICS_LIB_REL=../../DEPLOY/lib/libics.a \
-#	      -D CORE_TIFF_LIB_REL=../../DEPLOY/lib64/libtiff.a \
-#	      -D CORE_ZLIB_LIB_REL=../../DEPLOY/lib/libz.a \
+	      -D CORE_WITH_TARGA=OFF -D GLOBAL_BUILD_SHARED_LIBS=OFF \
+	      -D CORE_ICS_HEADERS_REL=$DEPLOYDIR/include \
+	      -D CORE_ICS_LIB_REL=$DEPLOYDIR/lib/libics.a \
+	      -D CORE_TIFF_HEADERS_REL=$DEPLOYDIR/include \
+	      -D CORE_TIFF_LIB_REL=$DEPLOYDIR/lib64/libtiff.a \
+	      -D CORE_ZLIB_HEADERS_REL=$DEPLOYDIR/include \
+	      -D CORE_ZLIB_LIB_REL=$DEPLOYDIR/lib/libz.a \
+	      -D ALGO_FFTW_HEADERS_REL=$DEPLOYDIR/include \
+	      -D ALGO_FFTW_LIB_REL=$DEPLOYDIR/lib64/libfftw3.a \
+	      -D ALGO_FFTW_THREADS_LIB_REL=$DEPLOYDIR/lib64/libfftw3_threads.a \
+	      -D ALGO_FFTWF_LIB_REL=$DEPLOYDIR/lib64/libfftw3f.a \
+	      -D ALGO_FFTWF_THREADS_LIB_REL=$DEPLOYDIR/lib64/libfftw3f_threads.a \
+	      -D ALGO_FFTWL_LIB_REL=$DEPLOYDIR/lib64/libfftw3l.a \
+	      -D ALGO_FFTWL_THREADS_LIB_REL=$DEPLOYDIR/lib64/libfftw3l_threads.a \
+	      -D ALGO_F2C_LIB_REL=$DEPLOYDIR/lib64/libf2c.a \
+	      -D ALGO_BLAS_LIB_REL=$DEPLOYDIR/lib64/libblas.a \
+	      -D ALGO_LAPACK_LIB_REL=$DEPLOYDIR/lib64/liblapack.a \
+	      -D GLOBAL_INSTALL_PATH=$DEPLOYDIR ..
 
 	make -j${MAKEJOBS}
 	make install
