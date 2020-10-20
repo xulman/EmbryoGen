42385b89ce1e6cc4778af56a21d93ed9d7ad7fcb
Vladimír Ulman <ulman@fi.muni.cz>
2020-10-20 18:43:26 +0200

    FIX: NucleusAgent::drawMask() is using Geometry::renderIntoMask()

--- src/Agents/NucleusAgent.cpp
+++ src/Agents/NucleusAgent.cpp
@@ -380,45 +380,5 @@ void NucleusAgent::drawForDebug(DisplayUnit& du)
 
 void NucleusAgent::drawMask(i3d::Image3d<i3d::GRAY16>& img)
 {
-	//shortcuts to the mask image parameters
-	const Vector3d<FLOAT> res(img.GetResolution().GetRes());
-	const Vector3d<FLOAT> off(img.GetOffset());
-
-	//shortcuts to our Own spheres
-	const Vector3d<FLOAT>* const centresO = futureGeometry.getCentres();
-	const FLOAT* const radiiO             = futureGeometry.getRadii();
-	const int iO                          = futureGeometry.getNoOfSpheres();
-
-	//project and "clip" this AABB into the img frame
-	//so that voxels to sweep can be narrowed down...
-	//
-	//   sweeping position and boundaries (relevant to the 'img')
-	Vector3d<size_t> curPos, minSweepPX,maxSweepPX;
-	futureGeometry.AABB.exportInPixelCoords(img, minSweepPX,maxSweepPX);
-	//
-	//micron coordinate of the running voxel 'curPos'
-	Vector3d<FLOAT> centre;
-
-	//sweep and check intersection with spheres' volumes
-	for (curPos.z = minSweepPX.z; curPos.z < maxSweepPX.z; curPos.z++)
-	for (curPos.y = minSweepPX.y; curPos.y < maxSweepPX.y; curPos.y++)
-	for (curPos.x = minSweepPX.x; curPos.x < maxSweepPX.x; curPos.x++)
-	{
-		//get micron coordinate of the current voxel's centre
-		centre.toMicronsFrom(curPos, res,off);
-
-		//check the current voxel against all spheres
-		for (int i = 0; i < iO; ++i)
-		{
-			if ((centre-centresO[i]).len() <= radiiO[i])
-			{
-#ifdef DEBUG
-				i3d::GRAY16 val = img.GetVoxel(curPos.x,curPos.y,curPos.z);
-				if (val > 0 && val != (i3d::GRAY16)ID)
-					REPORT(IDSIGN << " overwrites mask of " << val << " at " << curPos);
-#endif
-				img.SetVoxel(curPos.x,curPos.y,curPos.z, (i3d::GRAY16)ID);
-			}
-		}
-	}
+	futureGeometry.renderIntoMask(img,(i3d::GRAY16)ID);
 }
