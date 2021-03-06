711786013560cdb5eadcbcd555b15d46f2dff7ab
Vladimir Ulman <ulman@fi.muni.cz>
2021-03-06 08:33:12 +0100

    ADD: extended the script to prepare deps ADD: JPEG support in libs/prepareEverything.sh (cause TIFF      was asking for it on barbora), reflected that in CMakeLists CHG: TIFF and I3DLIBS are using JPEG now FIX: GSL asked not to create a shared lib (.so) FIX: GSL is downloaded via html, not ftp FIX: libs/addModulesAtIT4I ADD: module list for salomon @ IT4I

--- libs/prepareEverything.sh
+++ libs/prepareEverything.sh
@@ -2,16 +2,20 @@
 
 # --------- config ---------
 DEPLOYDIR=$PWD/DEPLOY
-MAKEJOBS=8
+MAKEJOBS=12
 
 prepare_ICS=YES
 prepare_GSL=YES
 prepare_LAPACKwithBLAS=YES
 prepare_ZLIB=YES
+prepare_JPEG=YES
 prepare_TIFF=YES
 prepare_FFTW=YES
 prepare_F2C=YES
 prepare_I3D=YES
+
+prepare_ZMQ=YES
+prepare_DAIS=YES
 
 
 mkdir -p $DEPLOYDIR
@@ -39,10 +43,10 @@ fi
 echo ========================= GSL =========================
 if [ "_$prepare_GSL" == "_YES" ]; then
 	GSL_archive=gsl-2.6.tar.gz
-	wget ftp://ftp.gnu.org/gnu/gsl/$GSL_archive
+	wget https://mirror.easyname.at/gnu/gsl/$GSL_archive
 	tar -xzf $GSL_archive
 	cd ${GSL_archive:0:7}
-	./configure  --prefix=$DEPLOYDIR
+	./configure  --prefix=$DEPLOYDIR --disable-shared
 	make -j${MAKEJOBS}
 	make install
 	cd ..
@@ -82,6 +86,19 @@ else
 	echo "not now"
 fi
 
+echo ========================= JPEG =========================
+if [ "_$prepare_JPEG" == "_YES" ]; then
+	wget https://www.ijg.org/files/jpegsrc.v9d.tar.gz
+	tar -xzf jpegsrc.v9d.tar.gz
+	cd jpeg-9d
+	./configure  --prefix=$DEPLOYDIR --disable-shared
+	make -j${MAKEJOBS}
+	make install
+	cd ..
+else
+	echo "not now"
+fi
+
 echo ========================= TIFF =========================
 if [ "_$prepare_TIFF" == "_YES" ]; then
 	TIFF_archive=tiff-4.0.10.tar.gz
@@ -90,7 +107,7 @@ if [ "_$prepare_TIFF" == "_YES" ]; then
 	cd ${TIFF_archive:0:11}
 	mkdir -p BUILD
 	cd BUILD
-	cmake -D BUILD_SHARED_LIBS=OFF -D jbig=OFF -D jpeg=OFF -D jpeg12=OFF -D webp=OFF -D zstd=OFF -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
+	cmake -D BUILD_SHARED_LIBS=OFF -D jbig=OFF -D jpeg12=OFF -D webp=OFF -D zstd=OFF -D lzma=OFF -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR ..
 	make -j${MAKEJOBS}
 	make install
 	cd ../..
@@ -140,10 +157,12 @@ if [ "_$prepare_I3D" == "_YES" ]; then
 	mkdir -p BUILD
 	cd BUILD
 	cmake -D ALGO_WITH_BLAS=ON -D ALGO_WITH_LAPACK=ON -D CORE_WITH_BIOFORMATS=OFF -D CORE_WITH_DCM=OFF \
-	      -D CORE_WITH_HDF5=OFF -D CORE_WITH_JPEG=OFF -D CORE_WITH_METAIO=OFF -D CORE_WITH_PNG=OFF \
+	      -D CORE_WITH_HDF5=OFF -D CORE_WITH_METAIO=OFF -D CORE_WITH_PNG=OFF \
 	      -D CORE_WITH_TARGA=OFF -D GLOBAL_BUILD_SHARED_LIBS=OFF \
 	      -D CORE_ICS_HEADERS_REL=$DEPLOYDIR/include \
 	      -D CORE_ICS_LIB_REL=$DEPLOYDIR/lib/libics.a \
+	      -D CORE_JPEG_HEADERS_REL=$DEPLOYDIR/include \
+	      -D CORE_JPEG_LIB_REL=$DEPLOYDIR/lib64/libjpeg.a \
 	      -D CORE_TIFF_HEADERS_REL=$DEPLOYDIR/include \
 	      -D CORE_TIFF_LIB_REL=$DEPLOYDIR/lib64/libtiff.a \
 	      -D CORE_ZLIB_HEADERS_REL=$DEPLOYDIR/include \
@@ -166,3 +185,65 @@ if [ "_$prepare_I3D" == "_YES" ]; then
 else
 	echo "not now"
 fi
+
+echo ========================= ZMQ =========================
+if [ "_$prepare_ZMQ" == "_YES" ]; then
+	git clone git://github.com/zeromq/libzmq.git
+	cd libzmq
+	mkdir BUILD
+	cd BUILD
+	cmake -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR -D BUILD_SHARED=OFF -D BUILD_TESTS=OFF -D WITH_DOCS=OFF ..
+	make -j${MAKEJOBS}
+	make install
+	cd ../..
+
+	git clone git://github.com/zeromq/cppzmq.git
+	cd cppzmq
+	mkdir BUILD
+	cd BUILD
+	cmake -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR -D ZeroMQ_DIR=$DEPLOYDIR/lib64/cmake/ZeroMQ -D CPPZMQ_BUILD_TESTS=OFF ..
+	make -j${MAKEJOBS}
+	make install
+	cd ../..
+else
+	echo "not now"
+fi
+
+echo ========================= DAIS =========================
+if [ "_$prepare_DAIS" == "_YES" ]; then
+	git clone git@github.com:xulman/DAIS-wp1.3.git
+	cd DAIS-wp1.3/C++/core
+	mkdir BUILD
+	cd BUILD
+	cmake -D CMAKE_INSTALL_PREFIX=$DEPLOYDIR -D ZeroMQ_DIR=$DEPLOYDIR/lib64/cmake/ZeroMQ ..
+	make -j${MAKEJOBS}
+	make install
+	cd ../../../..
+else
+	echo "not now"
+fi
+
+echo ========================= EmbryoGen Hints: =========================
+echo "Start Cmake configuration of EmbryoGen with:"
+echo "cmake -D ZeroMQ_DIR=$DEPLOYDIR/lib64/cmake/ZeroMQ \\"
+echo "      -D INC_CPPZMQ=$DEPLOYDIR/include \\"
+echo "      -D INC_DAISwp13=$DEPLOYDIR/include \\"
+echo "      -D LIB_DAISwp13=$DEPLOYDIR/lib64/libDAIS-wp13.a \\"
+echo "   __addPathToCmakeListsHere__"
+echo
+
+echo "Start Cmake configuration of EmbryoGen with non-system libs statically built-in:"
+echo "cmake -D ZeroMQ_DIR=$DEPLOYDIR/lib64/cmake/ZeroMQ \\"
+echo "      -D INC_I3D=$DEPLOYDIR/include \\"
+echo "      -D INC_GSL=$DEPLOYDIR/include \\"
+echo "      -D INC_CPPZMQ=$DEPLOYDIR/include \\"
+echo "      -D INC_DAISwp13=$DEPLOYDIR/include \\"
+echo "      -D LIB_DAISwp13=$DEPLOYDIR/lib64/libDAIS-wp13.a \\"
+echo "      -D SEARCH_ALL_DEPLIBS=ON \\"
+echo "      -D SEARCH_FOR_STATIC=ON \\"
+echo "      -D CMAKE_LIBRARY_PATH=$DEPLOYDIR/lib64 \\"
+echo "   __addPathToCmakeListsHere__"
+echo
+
+echo "That pre-sets the build, fine-tune the build now with:"
+echo "ccmake __addPathToCmakeListsHere__"
