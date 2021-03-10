6f77b0f880ec41695f6a9d1dacd8933f19036f88
Vladimír Ulman <ulman@fi.muni.cz>
2021-03-10 17:20:47 +0100

    ADD: util/ParallelList auto chunks adjusting code

--- src/util/ParallelList.hpp
+++ src/util/ParallelList.hpp
@@ -4,18 +4,77 @@
 #include "report.h"
 
 #ifdef _OPENMP
-	#define RUN_MODE " running in parallel"
+	#include <omp.h>
+	#define RUN_MODE "running in parallel"
 #else
-	#define RUN_MODE " running sequentially"
+	#define RUN_MODE "running sequentially"
+	int omp_get_max_threads() { return 1; }
 #endif
+
+/**
+ * This is a summary of params that influence how visitEveryObject*()
+ * functions shall behave. One can adjust the params here...
+ *
+ * The plan is that a certain TaskSize (a summary number of tasks that
+ * need to be computed) is split among 'numThreads' workers that work
+ * in parallel. The workers share the load equally, yielding a certain
+ * (ideal) number of tasks in total per worker. This number is additionally
+ * divided by 'wantedRoundsNum', or further reduced to make sure the
+ * number of tasks per worker is for sure below 'maxThreadLoad'.
+ *
+ * We are not using a fixed, pre-planned splitting of the TaskSize.
+ * Instead, each worker comes to take 'groupSize' (see visitEveryObject())
+ * number of tasks and, after it is done, it asks for another 'groupSize'.
+ * If one worker works a lot faster than the others, this schema allows it
+ * to take more in total (compared to waiting for the slower ones in the
+ * pre-planned model). A compromise between not having too small 'groupSizes'
+ * (and waiting for critical sections too often) on one hand and not doing its
+ * fair-share uninterrupted in one go on the other hand (having no ability to
+ * load balance), we plan the workers to interrupt their work 'wantedRoundsNum'-times.
+ * It could, however, create still way too large 'groupSizes' (an "atomic" block
+ * of work that could possibly take way too long of wall-time) so we limit its
+ * size to 'maxThreadLoad'.
+ *
+ * The 'numThreads' above is a maximum between the currently available threads
+ * in the system (as reported by the openMP itself) and user preference given
+ * in 'maxNumThreads' below.
+ */
+struct VisitingPolicy
+{
+public:
+	/** max number of workers (threads) to allow to work in parallel */
+	static const int maxNumThreads = 64;
+
+	/** in how many portions should a fair-share of work of one worker be be split into */
+	static const int wantedRoundsNum = 3;
+
+	/** no work of one worker should consists of more than this number of jobs */
+	static const int maxThreadLoad = 10000;
+
+	static int getGroupSizeOptimalToThisTaskSize(const int taskSize)
+	{
+		int numThreads = omp_get_max_threads();
+		if (numThreads > maxNumThreads) numThreads = maxNumThreads;
+
+		int fairShare = taskSize / (numThreads*wantedRoundsNum)  +1;
+		return (fairShare < maxThreadLoad ? fairShare : maxThreadLoad);
+	}
+};
+
 
 template <class CONTAINER_OF_ITEMS,typename ITEM_TYPE>
 void visitEveryObject(typename CONTAINER_OF_ITEMS::iterator listOfObjs_iter,
                       typename CONTAINER_OF_ITEMS::iterator listOfObjs_end,
                       const std::function< void(ITEM_TYPE&) >& visitingFun,
-                      const int groupSize = 100)
+                      int groupSize = 500)
 {
-	DEBUG_REPORT("RW workhorse" << RUN_MODE);
+	if (groupSize <= 0)
+	{
+		groupSize = VisitingPolicy::maxThreadLoad;
+		REPORT("No auto-adjustment of load is available, using default groupSize.");
+	}
+
+	DEBUG_REPORT("RW workhorse " << RUN_MODE << " with groupSize=" << groupSize);
 	#pragma omp parallel shared(listOfObjs_iter)
 	{
 		bool keepAdvancing = true;
@@ -54,13 +113,20 @@ void visitEveryObject(typename CONTAINER_OF_ITEMS::iterator listOfObjs_iter,
 	}
 }
 
+
 template <class CONTAINER_OF_ITEMS,typename ITEM_TYPE>
 void visitEveryObject(typename CONTAINER_OF_ITEMS::const_iterator listOfObjs_iter,
                       typename CONTAINER_OF_ITEMS::const_iterator listOfObjs_end,
                       const std::function< void(const ITEM_TYPE&) >& visitingFun,
-                      const int groupSize = 100)
+                      int groupSize = 500)
 {
-	DEBUG_REPORT("RO workhorse" << RUN_MODE);
+	if (groupSize <= 0)
+	{
+		groupSize = VisitingPolicy::maxThreadLoad;
+		REPORT("No auto-adjustment of load is available, using default groupSize.");
+	}
+
+	DEBUG_REPORT("RO workhorse " << RUN_MODE << " with groupSize=" << groupSize);
 	#pragma omp parallel shared(listOfObjs_iter)
 	{
 		bool keepAdvancing = true;
@@ -103,28 +169,30 @@ void visitEveryObject(typename CONTAINER_OF_ITEMS::const_iterator listOfObjs_ite
 template <typename C>
 void visitEveryObject(std::list<C>& listOfObjs,
                       const std::function< void(C&) >& visitingFun,
-                      const int groupSize = 100)
+                      const int groupSize = -1)
 {
-	DEBUG_REPORT("RW list-wrapper" << RUN_MODE);
+	DEBUG_REPORT("RW list-wrapper " << RUN_MODE);
 	visitEveryObject<std::list<C>,C>(
 			listOfObjs.begin(),
 			listOfObjs.end(),
 			visitingFun,
-			groupSize
+			groupSize > 0 ? groupSize
+			              : VisitingPolicy::getGroupSizeOptimalToThisTaskSize((int)listOfObjs.size())
 		);
 }
 
 template <typename C>
 void visitEveryObject_const(const std::list<C>& listOfObjs,
                             const std::function< void(const C&) >& visitingFun,
-                            const int groupSize = 100)
+                            const int groupSize = -1)
 {
-	DEBUG_REPORT("RO list-wrapper" << RUN_MODE);
+	DEBUG_REPORT("RO list-wrapper " << RUN_MODE);
 	visitEveryObject<std::list<C>,C>(
 			listOfObjs.begin(),
 			listOfObjs.end(),
 			visitingFun,
-			groupSize
+			groupSize > 0 ? groupSize
+			              : VisitingPolicy::getGroupSizeOptimalToThisTaskSize((int)listOfObjs.size())
 		);
 }
 
@@ -132,27 +200,29 @@ void visitEveryObject_const(const std::list<C>& listOfObjs,
 template <typename K,typename V>
 void visitEveryObject(std::map<K,V>& mapOfObjs,
                       const std::function< void(V&) >& visitingFun,
-                      const int groupSize = 100)
+                      const int groupSize = -1)
 {
-	DEBUG_REPORT("RW map-value-wrapper" << RUN_MODE);
+	DEBUG_REPORT("RW map-value-wrapper " << RUN_MODE);
 	visitEveryObject< std::map<K,V>,std::pair<const K,V> >(
 			mapOfObjs.begin(),
 			mapOfObjs.end(),
 			[&visitingFun](std::pair<const K,V>& item){ visitingFun(item.second); },
-			groupSize
+			groupSize > 0 ? groupSize
+			              : VisitingPolicy::getGroupSizeOptimalToThisTaskSize((int)mapOfObjs.size())
 		);
 }
 
 template <typename K,typename V>
 void visitEveryObject_const(const std::map<K,V>& mapOfObjs,
                             const std::function< void(const V&) >& visitingFun,
-                            const int groupSize = 100)
+                            const int groupSize = -1)
 {
-	DEBUG_REPORT("RO map-value-wrapper" << RUN_MODE);
+	DEBUG_REPORT("RO map-value-wrapper " << RUN_MODE);
 	visitEveryObject< std::map<K,V>,std::pair<const K,V> >(
 			mapOfObjs.cbegin(),
 			mapOfObjs.cend(),
 			[&visitingFun](const std::pair<const K,V>& item){ visitingFun(item.second); },
-			groupSize
+			groupSize > 0 ? groupSize
+			              : VisitingPolicy::getGroupSizeOptimalToThisTaskSize((int)mapOfObjs.size())
 		);
 }
