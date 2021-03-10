95c02c64e364bd937a54c2112219cc7720d3372f
Vladimír Ulman <ulman@fi.muni.cz>
2021-03-10 01:25:12 +0100

    CHG,EXT: src/util/ParallelList.hpp more generic, added aux walkers

--- src/util/ParallelList.hpp
+++ src/util/ParallelList.hpp
@@ -1,33 +1,120 @@
-#include <list>
 #include <functional>
+#include <list>
+#include <map>
+#include "report.h"
 
-template <class C>
-void visitEveryObject(std::list<C>& listOfObjs,
-                      const std::function< void(C) >& visitingFun)
+#ifdef _OPENMP
+	#define RUN_MODE " running in parallel"
+#else
+	#define RUN_MODE " running sequentially"
+#endif
+
+template <class CONTAINER_OF_ITEMS,typename ITEM_TYPE>
+void visitEveryObject(typename CONTAINER_OF_ITEMS::iterator listOfObjs_iter,
+                      typename CONTAINER_OF_ITEMS::iterator listOfObjs_end,
+                      const std::function< void(ITEM_TYPE&) >& visitingFun)
 {
-	typedef typename std::list<C>::iterator it_t;
-	it_t it = listOfObjs.begin();
-
-	#pragma omp parallel shared(it)
+	DEBUG_REPORT("RW workhorse" << RUN_MODE);
+	#pragma omp parallel shared(listOfObjs_iter)
 	{
-		bool doAdvance = true;
-		it_t local_it;
+		bool keepAdvancing = true;
+		typename CONTAINER_OF_ITEMS::iterator local_it;
 
 		do
 		{
 			#pragma omp critical
 			{
 				//if the list is not yet fully processed...
-				if (it != listOfObjs.end())
+				if (listOfObjs_iter != listOfObjs_end)
 					//...progress on next item,...
-					local_it = it++;
+					local_it = listOfObjs_iter++;
 				else
 					//...or just stay "relaxed"
-					doAdvance = false;
+					keepAdvancing = false;
 			}
 
-			if (doAdvance) visitingFun(*local_it);
+			if (keepAdvancing) visitingFun(*local_it);
 		}
-		while (doAdvance);
+		while (keepAdvancing);
 	}
+}
+
+template <class CONTAINER_OF_ITEMS,typename ITEM_TYPE>
+void visitEveryObject(typename CONTAINER_OF_ITEMS::const_iterator listOfObjs_iter,
+                      typename CONTAINER_OF_ITEMS::const_iterator listOfObjs_end,
+                      const std::function< void(const ITEM_TYPE&) >& visitingFun)
+{
+	DEBUG_REPORT("RO workhorse" << RUN_MODE);
+	#pragma omp parallel shared(listOfObjs_iter)
+	{
+		bool keepAdvancing = true;
+		typename CONTAINER_OF_ITEMS::const_iterator local_it;
+
+		do
+		{
+			#pragma omp critical
+			{
+				//if the list is not yet fully processed...
+				if (listOfObjs_iter != listOfObjs_end)
+					//...progress on next item,...
+					local_it = listOfObjs_iter++;
+				else
+					//...or just stay "relaxed"
+					keepAdvancing = false;
+			}
+
+			if (keepAdvancing) visitingFun(*local_it);
+		}
+		while (keepAdvancing);
+	}
+}
+
+
+template <typename C>
+void visitEveryObject(std::list<C>& listOfObjs,
+                      const std::function< void(C&) >& visitingFun)
+{
+	DEBUG_REPORT("RW list-wrapper" << RUN_MODE);
+	visitEveryObject<std::list<C>,C>(
+			listOfObjs.begin(),
+			listOfObjs.end(),
+			visitingFun
+		);
+}
+
+template <typename C>
+void visitEveryObject_const(const std::list<C>& listOfObjs,
+                            const std::function< void(const C&) >& visitingFun)
+{
+	DEBUG_REPORT("RO list-wrapper" << RUN_MODE);
+	visitEveryObject<std::list<C>,C>(
+			listOfObjs.begin(),
+			listOfObjs.end(),
+			visitingFun
+		);
+}
+
+
+template <typename K,typename V>
+void visitEveryObject(std::map<K,V>& mapOfObjs,
+                      const std::function< void(V&) >& visitingFun)
+{
+	DEBUG_REPORT("RW map-value-wrapper" << RUN_MODE);
+	visitEveryObject< std::map<K,V>,std::pair<const K,V> >(
+			mapOfObjs.begin(),
+			mapOfObjs.end(),
+			[&visitingFun](std::pair<const K,V>& item){ visitingFun(item.second); }
+		);
+}
+
+template <typename K,typename V>
+void visitEveryObject_const(const std::map<K,V>& mapOfObjs,
+                            const std::function< void(const V&) >& visitingFun)
+{
+	DEBUG_REPORT("RO map-value-wrapper" << RUN_MODE);
+	visitEveryObject< std::map<K,V>,std::pair<const K,V> >(
+			mapOfObjs.cbegin(),
+			mapOfObjs.cend(),
+			[&visitingFun](const std::pair<const K,V>& item){ visitingFun(item.second); }
+		);
 }
