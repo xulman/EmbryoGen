d9d06590207539297d41ddc43314ccb893a9a913
Jan Juracka <xjurack1@fi.muni.cz>
2022-09-22 17:19:56 +0200

    CHG: moved needed code to {Director,FO}.cpp and delete *SMP.cpp

--- src/Director.cpp
+++ src/Director.cpp
@@ -548,3 +548,67 @@ void Director::reportAgentsAllocation() {
 		report::message(
 		    fmt::format("agent ID {} at FO #{}", AgFO.first, AgFO.second));
 }
+
+
+// =================================== MOVED FROM DirectorSMP.cpp
+void Director::notify_publishAgentsAABBs(const int /* FOsID */) {
+	// notify the first FO
+	// MPI_signalThisEvent_to( FOsID );
+
+	// in the SMP world:
+	FO->updateAndPublishAgents();
+}
+
+void Director::waitFor_publishAgentsAABBs() {
+	// wait here until we're told (that the broadcasting is over)
+
+	// in MPI case, really do wait for the event to come
+}
+
+size_t Director::request_CntOfAABBs(const int /* FOsID */) {
+	return FO->getSizeOfAABBsList();
+}
+
+void Director::notify_setDetailedDrawingMode(const int /* FOsID */,
+                                             const int agentID,
+                                             const bool state) {
+	// in the SMP world:
+	FO->setAgentsDetailedDrawingMode(agentID, state);
+	//
+	// in the MPI, send the above to the agent FOsID
+}
+
+void Director::notify_setDetailedReportingMode(const int /* FOsID */,
+                                               const int agentID,
+                                               const bool state) {
+	// in the SMP world:
+	FO->setAgentsDetailedReportingMode(agentID, state);
+	//
+	// in the MPI, send the above to the agent FOsID
+}
+
+void Director::waitHereUntilEveryoneIsHereToo() {}
+
+void Director::request_renderNextFrame(const int /* FOsID */) {
+	// in the SMP:
+	FO->renderNextFrame();
+
+	// MPI world:
+	// this exactly the same code as in
+	// FrontOfficer::request_renderNextFrame(FOsID)
+}
+
+void Director::waitFor_renderNextFrame(const int /*FOsID*/) {
+	// does nothing in the SMP
+
+	// MPI world:
+	// this exactly the same code as in FrontOfficer::waitFor_renderNextFrame()
+}
+
+void Director::broadcast_setRenderingDebug(const bool setFlagToThis) {
+	// SMP:
+	FO->setSimulationDebugRendering(setFlagToThis);
+
+	// MPI world:
+	// non-blocking send out to everyone
+}
--- src/FrontOfficer.cpp
+++ src/FrontOfficer.cpp
@@ -550,3 +550,102 @@ void FrontOfficer::reportAABBs() {
 		    toString(naabb.minCorner), toString(naabb.maxCorner),
 		    agentsToFOsMap[naabb.ID]));
 }
+
+
+//=================================== ADDED FROM FrontOfficerSMP.cpp
+int FrontOfficer::request_getNextAvailAgentID() {
+	return Direktor->getNextAvailAgentID();
+}
+
+void FrontOfficer::request_startNewAgent(
+    const int newAgentID,
+    const int associatedFO,
+    const bool wantsToAppearInCTCtracksTXTfile) {
+	Direktor->startNewAgent(newAgentID, associatedFO,
+	                        wantsToAppearInCTCtracksTXTfile);
+}
+
+void FrontOfficer::request_closeAgent(const int agentID,
+                                      const int associatedFO) {
+	Direktor->closeAgent(agentID, associatedFO);
+}
+
+void FrontOfficer::request_updateParentalLink(const int childID,
+                                              const int parentID) {
+	Direktor->startNewDaughterAgent(childID, parentID);
+}
+
+void FrontOfficer::waitFor_publishAgentsAABBs() {
+	// wait here until we're told to start the broadcast which
+	// our agents were removed and AABBs of the remaining ones
+
+	// in MPI case, really do wait for the event to come
+}
+
+void FrontOfficer::notify_publishAgentsAABBs(const int /* FOsID */) {
+	// once our broadcasting is over, notify next FO to do the same
+	//(or notify the Direktor if we're the last on the round-robin-chain)
+
+	// in this SMP particular implementation we do nothing because
+	// there is no other FO, and the Direktor called us directly
+
+	// in MPI case, tell the next FO (or the Direktor)
+	// MPI_signalThisEvent_to( FOsID );
+}
+
+void FrontOfficer::broadcast_AABBofAgent(const ShadowAgent& ag) {
+	// this shall tell all (including this one) FOs the AABB of the given agent,
+	// the Direktor actually does not care
+
+	// in this SMP particular implementation we do only update ourselves,
+	// there is no other FO and Direktor doesn't care about this update
+	//
+	// this code essentially supplies the work of respond_AABBofAgent() in the
+	// SMP world
+	AABBs.emplace_back(ag.getAABB(), ag.getID(), ag.getAgentTypeID());
+	agentsAndBroadcastGeomVersions[ag.getID()] = ag.getGeometry().version;
+	registerThatThisAgentIsAtThisFO(ag.getID(), this->ID);
+
+	/*
+	//here, according to doc/agentTypeDictionary.txt
+	//this method shall have one extra param: noOfNewAgentTypes
+	//but it will not be used in this case
+	*/
+}
+
+ShadowAgent* FrontOfficer::request_ShadowAgentCopy(const int /* agentID */,
+                                                   const int /* FOsID */) {
+	// this never happens here (as there is no other FO to talk to)
+	return NULL;
+
+	// MPI world:
+	// contacts FO at FOsID telling it that it needs agentID's ShadowAgent back
+
+	// gives: agentID, this->ID (to tell the other party whom to talk back)
+	// waits
+	// gets : Geometry data, agentID and agentType
+
+	/* fake data!
+	Geometry*   gotThisGeom      = new Spheres(4);
+	int         gotThisAgentID   = 10;
+	std::string gotThisAgentType = "fake agent";
+	return new ShadowAgent(*gotThisGeom, gotThisAgentID,gotThisAgentType);
+	*/
+}
+
+void FrontOfficer::waitHereUntilEveryoneIsHereToo() {}
+
+void FrontOfficer::waitFor_renderNextFrame(const int /*FOsID*/) {
+	// normally, it should move the content of the images from this FO
+	// into Direktor's images... but instead we render directly into
+	// Direktor's images... see FrontOfficer::renderNextFrame()
+}
+
+void FrontOfficer::request_renderNextFrame(const int /* FOsID */) {
+	// does nothing in the SMP
+
+	// MPI world:
+
+	// this is the counterpart to the waitFor_renderNextFrame()
+	// one sends out the images in this order: mask, phantom, optics
+}
